b39606284eedb633a4c968885f0ead9c65cabf6e
Ahmad Sherif <me@ahmadsherif.com>
2016-11-29 20:46:51 +0200

    Modify TestServerStandingUp to pass

--- server/server_test.go
+++ server/server_test.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestServerStandingUp(t *testing.T) {
 	service := NewService()
 	address := "127.0.0.1:6666"
+
 	go service.Serve(address)
 	defer service.Stop()
+
+	// Give service a little time to start listening for connections
+	time.Sleep(10 * time.Millisecond)
 
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
