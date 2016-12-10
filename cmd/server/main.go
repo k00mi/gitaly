76972b7a84cf1bed8760a53bfecea8d2bf079b1a
Pablo Carranza <pcarranza@gmail.com>
2016-12-10 22:41:04 +0100

    Make the server command to actually stop gracefully

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"gitlab.com/gitlab-org/git-access-daemon/server"
 )
 
 func main() {
-	server.NewService().Serve("0.0.0.0:6666", server.CommandExecutorCallback)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+
+	service := server.NewService()
+	go func() {
+		service.Serve("0.0.0.0:6666", server.CommandExecutorCallback)
+	}()
+
+	select {
+	case <-ch:
+		service.Stop()
+	}
 }
