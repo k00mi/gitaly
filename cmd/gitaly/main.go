e32c0ad734100057234a2f9e2b255fc8ac3162d5
Ahmad Sherif <me@ahmadsherif.com>
2016-12-22 16:54:55 +0200

    Use envconfig package for configurations setting

--- cmd/gitaly/main.go
+++ cmd/gitaly/main.go
@@ -1,35 +1,41 @@
 package main
 
 import (
-	"flag"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	serv "gitlab.com/gitlab-org/gitaly/server"
 )
 
-var (
-	prometheusListenAddr = flag.String("prometheusListenAddr", "", "Prometheus listening address, e.g. ':9100'")
-)
+type Config struct {
+	PrometheusListenAddr string `split_words:"true"`
+}
 
 func main() {
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 
-	server := serv.NewServer()
+	config := Config{}
+	err := envconfig.Process("gitaly", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	go func() {
 		server.Serve("0.0.0.0:6666", serv.CommandExecutor)
 	}()
 
-	if *prometheusListenAddr != "" {
+	if config.PrometheusListenAddr != "" {
 		promMux := http.NewServeMux()
 		promMux.Handle("/metrics", promhttp.Handler())
 		go func() {
-			http.ListenAndServe(*prometheusListenAddr, promMux)
+			http.ListenAndServe(config.PrometheusListenAddr, promMux)
 		}()
 	}
 
