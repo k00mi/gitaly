66bfee57b52d770164574e0c7e35149af5db3c6c
Ahmad Sherif <me@ahmadsherif.com>
2017-03-01 18:55:44 +0200

    Use a helper function to test if a byte slice has a prefix out of a list

---
+++ internal/helper/byte.go
@@ -0,0 +1,16 @@
+package helper
+
+import (
+	"bytes"
+)
+
+// ByteSliceHasAnyPrefix tests whether the byte slice s begins with any of the prefixes.
+func ByteSliceHasAnyPrefix(s []byte, prefixes []string) bool {
+	for _, prefix := range prefixes {
+		if bytes.HasPrefix(s, []byte(prefix)) {
+			return true
+		}
+	}
+
+	return false
+}
