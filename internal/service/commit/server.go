7bd10812eacf8472e4326299102c4c4fe4a57c71
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2017-07-12 11:35:53 +0200

    CommitService.CommitStats
    - Vendor proto 0.16.0

--- internal/service/commit/server.go
+++ internal/service/commit/server.go
@@ -1,6 +1,8 @@
 package commit
 
 import (
+	"golang.org/x/net/context"
+
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
 )
 
@@ -9,4 +11,16 @@ type server struct{}
 // NewServer creates a new instance of a grpc CommitServiceServer
 func NewServer() pb.CommitServiceServer {
 	return &server{}
+}
+
+func (server) FindCommit(ctx context.Context, in *pb.FindCommitRequest) (*pb.FindCommitResponse, error) {
+	return nil, nil
+}
+
+func (server) GetTreeEntries(*pb.GetTreeEntriesRequest, pb.CommitService_GetTreeEntriesServer) error {
+	return nil
+}
+
+func (server) ListFiles(*pb.ListFilesRequest, pb.CommitService_ListFilesServer) error {
+	return nil
 }
