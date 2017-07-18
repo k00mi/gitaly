746f477b1a10b6ab7d91962e62a06a56d80532ff
Alejandro Rodríguez <alejorro70@gmail.com>
2017-07-18 15:27:18 -0400

    Implement CommitService.FindCommit

--- internal/service/commit/server.go
+++ internal/service/commit/server.go
@@ -1,8 +1,6 @@
 package commit
 
 import (
-	"golang.org/x/net/context"
-
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
 )
 
@@ -11,8 +9,4 @@ type server struct{}
 // NewServer creates a new instance of a grpc CommitServiceServer
 func NewServer() pb.CommitServiceServer {
 	return &server{}
-}
-
-func (server) FindCommit(ctx context.Context, in *pb.FindCommitRequest) (*pb.FindCommitResponse, error) {
-	return nil, nil
 }
