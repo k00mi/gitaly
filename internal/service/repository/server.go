8589baf249ebacf318a4cca589df47503b65a91b
Jacob Vosmaer (GitLab) <jacob@gitlab.com>
2017-09-20 08:47:42 +0000

    Use gitaly-proto 0.34.0

--- internal/service/repository/server.go
+++ internal/service/repository/server.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/helper"
+
+	"golang.org/x/net/context"
 )
 
 type server struct{}
@@ -9,4 +12,8 @@ type server struct{}
 // NewServer creates a new instance of a gRPC repo server
 func NewServer() pb.RepositoryServiceServer {
 	return &server{}
+}
+
+func (*server) CreateRepository(context.Context, *pb.CreateRepositoryRequest) (*pb.CreateRepositoryResponse, error) {
+	return nil, helper.Unimplemented
 }
