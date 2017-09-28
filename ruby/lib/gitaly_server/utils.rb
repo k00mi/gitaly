91a3e2fcde7eede443035654d3a2e9d84eb23bc3
Rémy Coutable <remy@rymai.me>
2017-09-28 17:43:07 +0000

    Use gitlab-styles

--- ruby/lib/gitaly_server/utils.rb
+++ ruby/lib/gitaly_server/utils.rb
@@ -3,11 +3,11 @@ module GitalyServer
     def gitaly_commit_from_rugged(rugged_commit)
       Gitaly::GitCommit.new(
         id: rugged_commit.oid,
-	subject: rugged_commit.message.split("\n", 2)[0].chomp.b,
-	body: rugged_commit.message.b,
+        subject: rugged_commit.message.split("\n", 2)[0].chomp.b,
+        body: rugged_commit.message.b,
         parent_ids: rugged_commit.parent_ids,
         author: gitaly_commit_author_from_rugged(rugged_commit.author),
-        committer: gitaly_commit_author_from_rugged(rugged_commit.committer),
+        committer: gitaly_commit_author_from_rugged(rugged_commit.committer)
       )
     end
 
