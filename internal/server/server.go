e0fe54139555bb896050761edf9b32dac799645d
Andrew Newdigate <andrew@gitlab.com>
2017-10-30 12:29:47 +0000

    Enable log tagging on client streamed requests

--- internal/server/server.go
+++ internal/server/server.go
@@ -27,7 +27,7 @@ func New(rubyServer *rubyserver.Server) *grpc.Server {
 	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
 
 	ctxTagOpts := []grpc_ctxtags.Option{
-		grpc_ctxtags.WithFieldExtractor(fieldextractors.RepositoryFieldExtractor),
+		grpc_ctxtags.WithFieldExtractorForInitialReq(fieldextractors.RepositoryFieldExtractor),
 	}
 
 	lh := limithandler.New()
