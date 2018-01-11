395d1a2417ab263d81afd32a25a666b2bd015f8f
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2018-01-11 07:34:48 +0100

    RepositoryService::WriteRef

--- internal/service/repository/write_ref.go
+++ internal/service/repository/write_ref.go
@@ -1,13 +1,51 @@
 package repository
 
 import (
-	"gitlab.com/gitlab-org/gitaly/internal/helper"
+	"bytes"
+	"fmt"
 
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/git"
+	"gitlab.com/gitlab-org/gitaly/internal/rubyserver"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 
 	"golang.org/x/net/context"
 )
 
 func (s *server) WriteRef(ctx context.Context, req *pb.WriteRefRequest) (*pb.WriteRefResponse, error) {
-	return nil, helper.Unimplemented
+	if err := validateWriteRefRequest(req); err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "WriteRef: %v", err)
+	}
+
+	client, err := s.RepositoryServiceClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	clientCtx, err := rubyserver.SetHeaders(ctx, req.GetRepository())
+	if err != nil {
+		return nil, err
+	}
+
+	return client.WriteRef(clientCtx, req)
+}
+
+func validateWriteRefRequest(req *pb.WriteRefRequest) error {
+	if err := git.ValidateRevision(req.Ref); err != nil {
+		return fmt.Errorf("Validate Ref: %v", err)
+	}
+	if err := git.ValidateRevision(req.Revision); err != nil {
+		return fmt.Errorf("Validate Revision: %v", err)
+	}
+	if len(req.OldRevision) > 0 {
+		if err := git.ValidateRevision(req.OldRevision); err != nil {
+			return fmt.Errorf("Validate OldRevision: %v", err)
+		}
+	}
+
+	if !bytes.HasPrefix(req.Ref, []byte("refs/")) {
+		return fmt.Errorf("Ref has to be a full reference")
+	}
+	return nil
 }
---
+++ internal/service/repository/write_ref_test.go
@@ -0,0 +1,145 @@
+package repository
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
+	"google.golang.org/grpc/codes"
+)
+
+func TestWriteRefSuccessful(t *testing.T) {
+	server, serverSocketPath := runRepoServer(t)
+	defer server.Stop()
+
+	client, conn := newRepositoryClient(t, serverSocketPath)
+	defer conn.Close()
+
+	testRepo, testRepoPath, cleanupFn := testhelper.NewTestRepo(t)
+	defer cleanupFn()
+
+	testCases := []struct {
+		desc string
+		req  *pb.WriteRefRequest
+	}{
+		{
+			desc: "rugged update refs/heads/master",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/heads/master"),
+				Revision:   []byte("4a24d82dbca5c11c61556f3b35ca472b7463187e"),
+				Shell:      false,
+			},
+		},
+		{
+			desc: "shell update refs/heads/master",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/heads/master"),
+				Revision:   []byte("b83d6e391c22777fca1ed3012fce84f633d7fed0"),
+				Shell:      true,
+			},
+		},
+		{
+			desc: "shell update refs/heads/master w/ validation",
+			req: &pb.WriteRefRequest{
+				Repository:  testRepo,
+				Ref:         []byte("refs/heads/master"),
+				Revision:    []byte("498214de67004b1da3d820901307bed2a68a8ef6"),
+				OldRevision: []byte("b83d6e391c22777fca1ed3012fce84f633d7fed0"),
+				Shell:       true,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctx, cancel := testhelper.Context()
+			defer cancel()
+			_, err := client.WriteRef(ctx, tc.req)
+
+			require.NoError(t, err)
+
+			rev := testhelper.MustRunCommand(t, nil, "git", "--git-dir", testRepoPath, "log", "--pretty=%H", "-1", string(tc.req.Ref))
+
+			rev = bytes.Replace(rev, []byte("\n"), nil, 1)
+
+			require.Equal(t, string(tc.req.Revision), string(rev))
+		})
+	}
+}
+
+func TestWriteRefValidationError(t *testing.T) {
+	server, serverSocketPath := runRepoServer(t)
+	defer server.Stop()
+
+	client, conn := newRepositoryClient(t, serverSocketPath)
+	defer conn.Close()
+
+	testRepo, _, cleanupFn := testhelper.NewTestRepo(t)
+	defer cleanupFn()
+
+	testCases := []struct {
+		desc string
+		req  *pb.WriteRefRequest
+	}{
+		{
+			desc: "empty revision",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/heads/master"),
+			},
+		},
+		{
+			desc: "empty ref name",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Revision:   []byte("498214de67004b1da3d820901307bed2a68a8ef6"),
+			},
+		},
+		{
+			desc: "non-prefixed ref name",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("master"),
+				Revision:   []byte("498214de67004b1da3d820901307bed2a68a8ef6"),
+			},
+		},
+		{
+			desc: "revision contains \\x00",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/heads/master"),
+				Revision:   []byte("012301230123\x001243"),
+			},
+		},
+		{
+			desc: "ref contains \\x00",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/head\x00s/master\x00"),
+				Revision:   []byte("0123012301231243"),
+			},
+		},
+		{
+			desc: "ref contains whitespace",
+			req: &pb.WriteRefRequest{
+				Repository: testRepo,
+				Ref:        []byte("refs/heads /master"),
+				Revision:   []byte("0123012301231243"),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctx, cancel := testhelper.Context()
+			defer cancel()
+			_, err := client.WriteRef(ctx, tc.req)
+
+			testhelper.AssertGrpcError(t, err, codes.InvalidArgument, "")
+		})
+	}
+}
