5680860e39e2f04fe3373bfe96fc6d297a4384ff
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2018-01-18 22:30:25 +0100

    Fix gRPC update snafu

--- internal/service/repository/create_bundle.go
+++ internal/service/repository/create_bundle.go
@@ -8,21 +8,21 @@ import (
 
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *server) CreateBundle(req *pb.CreateBundleRequest, stream pb.RepositoryService_CreateBundleServer) error {
 	repo := req.GetRepository()
 	if repo == nil {
-		return grpc.Errorf(codes.InvalidArgument, "CreateBundle: empty Repository")
+		return status.Errorf(codes.InvalidArgument, "CreateBundle: empty Repository")
 	}
 
 	ctx := stream.Context()
 
 	cmd, err := git.Command(ctx, repo, "bundle", "create", "-", "--all")
 	if err != nil {
-		return grpc.Errorf(codes.Internal, "CreateBundle: cmd start failed: %v", err)
+		return status.Errorf(codes.Internal, "CreateBundle: cmd start failed: %v", err)
 	}
 
 	writer := streamio.NewWriter(func(p []byte) error {
@@ -31,11 +31,11 @@ func (s *server) CreateBundle(req *pb.CreateBundleRequest, stream pb.RepositoryS
 
 	_, err = io.Copy(writer, cmd)
 	if err != nil {
-		return grpc.Errorf(codes.Internal, "CreateBundle: stream writer failed: %v", err)
+		return status.Errorf(codes.Internal, "CreateBundle: stream writer failed: %v", err)
 	}
 
 	if err := cmd.Wait(); err != nil {
-		return grpc.Errorf(codes.Internal, "CreateBundle: cmd wait failed: %v", err)
+		return status.Errorf(codes.Internal, "CreateBundle: cmd wait failed: %v", err)
 	}
 
 	return nil
