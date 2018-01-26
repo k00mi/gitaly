dc55b0c0a8dc3f54552884c5931aedbb234e6df4
Zeger-Jan van de Weg <git@zjvandeweg.nl>
2018-01-26 12:25:36 +0100

    :monkey: patch gollum tree_entry
    This ports behaviour from gitlab-org/gitlab-ce!16197

--- ruby/lib/gitlab/gollum.rb
+++ ruby/lib/gitlab/gollum.rb
@@ -32,6 +32,26 @@ module Gollum
       end
     end
   end
+
+  module Git
+    class Git
+      def tree_entry(commit, path)
+        pathname = Pathname.new(path)
+        tmp_entry = nil
+
+        pathname.each_filename do |dir|
+          tmp_entry = if tmp_entry.nil?
+                        commit.tree[dir]
+                      else
+                        @repo.lookup(tmp_entry[:oid])[dir]
+                      end
+
+          return nil unless tmp_entry
+        end
+        tmp_entry
+      end
+    end
+  end
 end
 
 Gollum::Page.per_page = 20 # Magic number from Kaminari.config.default_per_page
