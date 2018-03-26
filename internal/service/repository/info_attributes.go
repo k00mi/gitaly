e1c5133b3d2a7f01dbbd7123d7cfa25d484ab5e1
Zeger-Jan van de Weg <git@zjvandeweg.nl>
2018-03-26 14:37:36 +0200

    Server Implementation GetInfoAttributes
    Part of https://gitlab.com/gitlab-org/gitaly/issues/1082

---
+++ internal/service/repository/info_attributes.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"io"
+	"os"
+	"path"
+
+	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/helper"
+	"gitlab.com/gitlab-org/gitaly/streamio"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *server) GetInfoAttributes(in *pb.GetInfoAttributesRequest, stream pb.RepositoryService_GetInfoAttributesServer) error {
+	repoPath, err := helper.GetRepoPath(in.GetRepository())
+	if err != nil {
+		return err
+	}
+
+	attrFile := path.Join(repoPath, "info", "attributes")
+	f, err := os.Open(attrFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			stream.Send(&pb.GetInfoAttributesResponse{})
+			return nil
+		}
+
+		return status.Errorf(codes.Internal, "GetInfoAttributes failure to read info attributes: %v", err)
+	}
+
+	sw := streamio.NewWriter(func(p []byte) error {
+		return stream.Send(&pb.GetInfoAttributesResponse{
+			Attributes: p,
+		})
+	})
+
+	_, err = io.Copy(sw, f)
+	return err
+}
