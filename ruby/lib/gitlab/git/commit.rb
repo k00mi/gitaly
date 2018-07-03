87ee0be2c5e8d9f4b458f4a12d9e4f7926448277
Jacob Vosmaer (GitLab) <jacob@gitlab.com>
2018-07-03 10:51:18 +0000

    Vendor gitlab-git at 7e9f46d0dc1ed34d7

--- ruby/lib/gitlab/git/commit.rb
+++ ruby/lib/gitlab/git/commit.rb
@@ -1,6 +1,18 @@
 module Gitlab
   module Git
     class Commit
+      class << self
+        def shas_with_signatures(repository, shas)
+          shas.select do |sha|
+            begin
+              Rugged::Commit.extract_signature(repository.rugged, sha)
+            rescue Rugged::OdbError
+              false
+            end
+          end
+        end
+      end
+
       def to_diff
         rugged_diff_from_parent.patch
       end
