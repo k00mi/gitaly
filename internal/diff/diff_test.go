422c3a57dbe9a80ec6c4fb778e8dd0d138a72c28
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-08-14 16:13:50 -0300

    Prune large patches by default

--- internal/diff/diff_test.go
+++ internal/diff/diff_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDiffParserWithLargeDiff(t *testing.T) {
+func TestDiffParserWithLargeDiffWithTrueCollapseDiffsFlag(t *testing.T) {
 	bigPatch := strings.Repeat("+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n", 100000)
 	rawDiff := fmt.Sprintf(`:000000 100644 0000000000000000000000000000000000000000 4cc7061661b8f52891bc1b39feb4d856b21a1067 A	big.txt
 :000000 100644 0000000000000000000000000000000000000000 3be11c69355948412925fa5e073d76d58ff3afd2 A	file-00.txt
@@ -51,6 +51,73 @@ index 0000000000000000000000000000000000000000..3be11c69355948412925fa5e073d76d5
 			Status:    'A',
 			Collapsed: true,
 			lineCount: 100000,
+		},
+		&Diff{
+			OldMode:   0,
+			NewMode:   0100644,
+			FromID:    "0000000000000000000000000000000000000000",
+			ToID:      "3be11c69355948412925fa5e073d76d58ff3afd2",
+			FromPath:  []byte("file-00.txt"),
+			ToPath:    []byte("file-00.txt"),
+			Status:    'A',
+			Collapsed: false,
+			Patch:     []byte("@@ -0,0 +1 @@\n+Lorem ipsum\n"),
+			lineCount: 1,
+		},
+	}
+
+	require.Equal(t, expectedDiffs, diffs)
+}
+
+func TestDiffParserWithLargeDiffWithFalseCollapseDiffsFlag(t *testing.T) {
+	bigPatch := strings.Repeat("+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n", 100000)
+	rawDiff := fmt.Sprintf(`:000000 100644 0000000000000000000000000000000000000000 4cc7061661b8f52891bc1b39feb4d856b21a1067 A	big.txt
+:000000 100644 0000000000000000000000000000000000000000 3be11c69355948412925fa5e073d76d58ff3afd2 A	file-00.txt
+
+diff --git a/big.txt b/big.txt
+new file mode 100644
+index 0000000000000000000000000000000000000000..4cc7061661b8f52891bc1b39feb4d856b21a1067
+--- /dev/null
++++ b/big.txt
+@@ -0,0 +1,100000 @@
+%sdiff --git a/file-00.txt b/file-00.txt
+new file mode 100644
+index 0000000000000000000000000000000000000000..3be11c69355948412925fa5e073d76d58ff3afd2
+--- /dev/null
++++ b/file-00.txt
+@@ -0,0 +1 @@
++Lorem ipsum
+`, bigPatch)
+
+	limits := Limits{
+		EnforceLimits: true,
+		SafeMaxFiles:  3,
+		SafeMaxBytes:  200,
+		SafeMaxLines:  200,
+		MaxFiles:      4,
+		MaxBytes:      10000000,
+		MaxLines:      10000000,
+		CollapseDiffs: false,
+	}
+	diffParser := NewDiffParser(strings.NewReader(rawDiff), limits)
+
+	diffs := []*Diff{}
+	for diffParser.Parse() {
+		diffs = append(diffs, diffParser.Diff())
+	}
+
+	expectedDiffs := []*Diff{
+		&Diff{
+			OldMode:   0,
+			NewMode:   0100644,
+			FromID:    "0000000000000000000000000000000000000000",
+			ToID:      "4cc7061661b8f52891bc1b39feb4d856b21a1067",
+			FromPath:  []byte("big.txt"),
+			ToPath:    []byte("big.txt"),
+			Status:    'A',
+			Collapsed: false,
+			lineCount: 100000,
+			TooLarge:  true,
 		},
 		&Diff{
 			OldMode:   0,
--- internal/service/diff/commit.go
+++ internal/service/diff/commit.go
@@ -77,6 +77,7 @@ func (s *server) CommitDiff(in *pb.CommitDiffRequest, stream pb.DiffService_Comm
 			Binary:         diff.Binary,
 			OverflowMarker: diff.OverflowMarker,
 			Collapsed:      diff.Collapsed,
+			TooLarge:       diff.TooLarge,
 		}
 
 		if len(diff.Patch) <= s.MsgSizeThreshold {
