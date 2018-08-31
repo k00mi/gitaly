8bdef42945333cc56ede41c72a56fd28bd8edeea
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-08-31 17:23:42 -0300

    Add server implementation for FindRemoteRootRef

---
+++ internal/service/remote/find_remote_root_ref_test.go
@@ -0,0 +1,95 @@
+package remote
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
+	"google.golang.org/grpc/codes"
+)
+
+func TestFindRemoteRootRefSuccess(t *testing.T) {
+	server, serverSocketPath := runRemoteServiceServer(t)
+	defer server.Stop()
+
+	client, conn := NewRemoteClient(t, serverSocketPath)
+	defer conn.Close()
+
+	testRepo, _, cleanupFn := testhelper.NewTestRepo(t)
+	defer cleanupFn()
+
+	request := &pb.FindRemoteRootRefRequest{Repository: testRepo, Remote: "origin"}
+	testCtx, cancelCtx := testhelper.Context()
+	defer cancelCtx()
+
+	response, err := client.FindRemoteRootRef(testCtx, request)
+	require.NoError(t, err)
+	require.Equal(t, "master", response.Ref)
+}
+
+func TestFindRemoteRootRefFailedDueToValidation(t *testing.T) {
+	server, serverSocketPath := runRemoteServiceServer(t)
+	defer server.Stop()
+
+	client, conn := NewRemoteClient(t, serverSocketPath)
+	defer conn.Close()
+
+	invalidRepo := &pb.Repository{StorageName: "fake", RelativePath: "path"}
+
+	testRepo, _, cleanupFn := testhelper.NewTestRepo(t)
+	defer cleanupFn()
+
+	testCases := []struct {
+		desc    string
+		request *pb.FindRemoteRootRefRequest
+		code    codes.Code
+	}{
+		{
+			desc:    "Invalid repository",
+			request: &pb.FindRemoteRootRefRequest{Repository: invalidRepo},
+			code:    codes.InvalidArgument,
+		},
+		{
+			desc:    "Repository is nil",
+			request: &pb.FindRemoteRootRefRequest{},
+			code:    codes.InvalidArgument,
+		},
+		{
+			desc:    "Remote is nil",
+			request: &pb.FindRemoteRootRefRequest{Repository: testRepo},
+			code:    codes.InvalidArgument,
+		},
+		{
+			desc:    "Remote is empty",
+			request: &pb.FindRemoteRootRefRequest{Repository: testRepo, Remote: ""},
+			code:    codes.InvalidArgument,
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCtx, cancelCtx := testhelper.Context()
+		defer cancelCtx()
+
+		_, err := client.FindRemoteRootRef(testCtx, testCase.request)
+		testhelper.RequireGrpcError(t, err, testCase.code)
+	}
+}
+
+func TestFindRemoteRootRefFailedDueToInvalidRemote(t *testing.T) {
+	server, serverSocketPath := runRemoteServiceServer(t)
+	defer server.Stop()
+
+	client, conn := NewRemoteClient(t, serverSocketPath)
+	defer conn.Close()
+
+	testRepo, _, cleanupFn := testhelper.NewTestRepo(t)
+	defer cleanupFn()
+
+	request := &pb.FindRemoteRootRefRequest{Repository: testRepo, Remote: "invalid"}
+	testCtx, cancelCtx := testhelper.Context()
+	defer cancelCtx()
+
+	_, err := client.FindRemoteRootRef(testCtx, request)
+	testhelper.RequireGrpcError(t, err, codes.Internal)
+}
