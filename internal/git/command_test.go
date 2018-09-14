244902bd255411913d8666e8f2d7590c4542b495
Stan Hu <stanhu@gmail.com>
2018-09-14 09:30:44 -0700

    Export HTTP proxy environment variables to Gitaly
    Git passes the all_proxy, no_proxy, http_proxy, and https_proxy
    environment variables to libcurl for HTTP requests.
    If these aren't set, repository imports among other git activity
    will fail behind a proxy.
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/51517

---
+++ internal/git/command_test.go
@@ -0,0 +1,39 @@
+package git
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGitCommandProxy(t *testing.T) {
+	requestReceived := false
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestReceived = true
+	}))
+	defer ts.Close()
+
+	oldHTTPProxy := os.Getenv("http_proxy")
+	defer os.Setenv("http_proxy", oldHTTPProxy)
+
+	os.Setenv("http_proxy", ts.URL)
+
+	ctx, _ := context.WithCancel(context.Background())
+
+	dir, err := ioutil.TempDir("", "test-clone")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	cmd, err := CommandWithoutRepo(ctx, "clone", "http://gitlab.com/bogus-repo", dir)
+	require.NoError(t, err)
+
+	err = cmd.Wait()
+	require.NoError(t, err)
+	require.True(t, requestReceived)
+}
