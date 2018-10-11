c965b2ef585314860ce88b8a80ae5916c22f6964
Alejandro Rodríguez <alejorro70@gmail.com>
2018-10-11 17:16:29 -0300

    Add Gitlab::Git::Branch unit specs

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/gitaly_server.rb'
 require_relative '../lib/gitlab/git.rb'
 require_relative 'support/sentry.rb'
+require 'timecop'
 require 'test_repo_helper'
 
 Dir[File.join(__dir__, 'support/helpers/*.rb')].each { |f| require f }
