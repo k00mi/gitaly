4c7a3a7b512ba701d70fdccc2ab210a9bce7ed76
Paul Okstad <pokstad@gitlab.com>
2019-03-01 20:07:12 +0000

    Merge branch 'master' into 'po_praefect_test_coverage'
    # Conflicts:
    #   internal/praefect/server_test.go

---
+++ internal/praefect/mocksvc_test.go
@@ -0,0 +1,23 @@
+package praefect_test
+
+import (
+	"context"
+
+	"gitlab.com/gitlab-org/gitaly/internal/praefect/mock"
+)
+
+type simpleUnaryUnaryCallback func(context.Context, *mock.SimpleRequest) (*mock.SimpleResponse, error)
+
+// mockSvc is an implementation of mock.SimpleServer for testing purposes. The
+// gRPC stub can be updated via go generate:
+//
+//go:generate protoc --go_out=plugins=grpc:. mock/mock.proto
+//go:generate goimports -w mock/mock.pb.go
+type mockSvc struct {
+	simpleUnaryUnary simpleUnaryUnaryCallback
+}
+
+// SimpleUnaryUnary is implemented by a callback
+func (m *mockSvc) SimpleUnaryUnary(ctx context.Context, req *mock.SimpleRequest) (*mock.SimpleResponse, error) {
+	return m.simpleUnaryUnary(ctx, req)
+}
