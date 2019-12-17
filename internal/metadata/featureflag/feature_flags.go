2ca7bec0f685e8f01d9570ec45399ad5c94bdd18
jramsay <pstrokov@gitlab.com>
2019-12-17 11:02:16 +0200

    Cleanup for the feature flag protection of the FilterShasWithSignatures method
    Feature flag removed.
    Clean up of go code that triggers Gitaly-Ruby code.
    Removal of related Prometheus metrics
    
    Part of: https://gitlab.com/gitlab-org/gitaly/issues/2268

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -4,8 +4,6 @@ const (
 	// UploadPackFilter enables partial clones by sending uploadpack.allowFilter and uploadpack.allowAnySHA1InWant
 	// to upload-pack
 	UploadPackFilter = "upload_pack_filter"
-	// FilterShasWithSignaturesGo will cause the FilterShasWithSignatures RPC to use the go implementation when set
-	FilterShasWithSignaturesGo = "filter_shas_with_signatures_go"
 	// LinguistFileCountStats will invoke an additional git-linguist command to get the number of files per language
 	LinguistFileCountStats = "linguist_file_count_stats"
 	// HooksRPC will invoke update, pre receive, and post receive hooks by using RPCs
