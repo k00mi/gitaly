18a4ef91bb1534bd3d806bc5e93390f2e9f01d73
Michael Hofmann <michael.hofmann@bitgestalt.com>
2020-01-08 13:29:59 +0000

    Fix DiskStatistics on OpenBSD.
    DiskStatistics in internal/service/server uses unix.Statfs to determine
    available storage space. Since the fields of unix.Statfs_t on OpenBSD
    have different names than on other Unices, a platform-specific
    implementation is necessary.

---
+++ internal/service/server/storage_status_openbsd.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"gitlab.com/gitlab-org/gitaly/internal/config"
+	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+	"golang.org/x/sys/unix"
+)
+
+func getStorageStatus(shard config.Storage) (*gitalypb.DiskStatisticsResponse_StorageStatus, error) {
+	var stats unix.Statfs_t
+	err := unix.Statfs(shard.Path, &stats)
+	if err != nil {
+		return nil, err
+	}
+
+	// Redundant conversions to handle differences between unix families
+	available := int64(stats.F_bavail) * int64(stats.F_bsize)
+	used := (int64(stats.F_blocks) - int64(stats.F_bfree)) * int64(stats.F_bsize)
+
+	return &gitalypb.DiskStatisticsResponse_StorageStatus{
+		StorageName: shard.Name,
+		Available:   available,
+		Used:        used,
+	}, nil
+}
