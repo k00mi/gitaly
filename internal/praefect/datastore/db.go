e8d3b93570e541492df616cb277216f5947abaef
Jacob Vosmaer <jacob@gitlab.com>
2020-01-14 21:01:59 +0000

    Add sql-migrate subcommand to Praefect

--- internal/praefect/datastore/db.go
+++ internal/praefect/datastore/db.go
@@ -8,14 +8,16 @@ import (
 
 	// Blank import to enable integration of github.com/lib/pq into database/sql
 	_ "github.com/lib/pq"
+	migrate "github.com/rubenv/sql-migrate"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/config"
+	"gitlab.com/gitlab-org/gitaly/internal/praefect/datastore/migrations"
 )
 
 // CheckPostgresVersion checks the server version of the Postgres DB
 // specified in conf. This is a diagnostic for the Praefect Postgres
 // rollout. https://gitlab.com/gitlab-org/gitaly/issues/1755
 func CheckPostgresVersion(conf config.Config) error {
-	db, err := sql.Open("postgres", conf.DB.ToPQString())
+	db, err := openDB(conf)
 	if err != nil {
 		return fmt.Errorf("sql open: %v", err)
 	}
@@ -35,4 +37,19 @@ func CheckPostgresVersion(conf config.Config) error {
 	}
 
 	return nil
+}
+
+func openDB(conf config.Config) (*sql.DB, error) { return sql.Open("postgres", conf.DB.ToPQString()) }
+
+// Migrate will apply all pending SQL migrations
+func Migrate(conf config.Config) (int, error) {
+	db, err := openDB(conf)
+	if err != nil {
+		return 0, fmt.Errorf("sql open: %v", err)
+	}
+	defer db.Close()
+
+	migrationSource := &migrate.MemoryMigrationSource{Migrations: migrations.All()}
+
+	return migrate.Exec(db, "postgres", migrationSource, migrate.Up)
 }
