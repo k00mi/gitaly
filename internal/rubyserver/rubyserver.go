73da5f49d3a94780dc8835c83d512ba05ae103c3
John Cai <jcai@gitlab.com>
2020-04-07 14:24:57 -0700

    Call hook rpcs from operations service

--- internal/rubyserver/rubyserver.go
+++ internal/rubyserver/rubyserver.go
@@ -96,6 +96,7 @@ func (s *Server) start() error {
 		"GITALY_RUBY_DIR="+cfg.Ruby.Dir,
 		"GITALY_VERSION="+version.GetVersion(),
 		"GITALY_GIT_HOOKS_DIR="+hooks.Path(),
+		"GITALY_SOCKET="+config.GitalyInternalSocketPath(),
 		"GITALY_RUGGED_GIT_CONFIG_SEARCH_PATH="+cfg.Ruby.RuggedGitConfigSearchPath)
 	env = append(env, gitlabshell.Env()...)
 
