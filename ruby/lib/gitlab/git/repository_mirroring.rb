f868b5a30ce9edd910a931721cc40ce89e31f918
Robert Speicher <rspeicher@gmail.com>
2020-04-21 17:18:56 -0500

    RepositoryMirroring lists heads and tags on remote
    This renames the private method `list_remote_tags` to
    `list_remote_refs`, and it will return the list of heads and tags on a
    given remote.

--- ruby/lib/gitlab/git/repository_mirroring.rb
+++ ruby/lib/gitlab/git/repository_mirroring.rb
@@ -51,7 +51,7 @@ module Gitlab
       def remote_tags(remote, env: {})
         # Each line has this format: "dc872e9fa6963f8f03da6c8f6f264d0845d6b092\trefs/tags/v1.10.0\n"
         # We want to convert it to: [{ 'v1.10.0' => 'dc872e9fa6963f8f03da6c8f6f264d0845d6b092' }, ...]
-        list_remote_tags(remote, env: env).map do |line|
+        list_remote_refs(remote, env: env).map do |line|
           target, refname = line.strip.split("\t")
 
           # When the remote repo does not have tags.
@@ -59,6 +59,8 @@ module Gitlab
             Rails.logger.info "Empty or invalid list of tags for remote: #{remote}"
             break []
           end
+
+          next unless refname.start_with?('refs/tags/')
 
           # We're only interested in tag references
           # See: http://stackoverflow.com/questions/15472107/when-listing-git-ls-remote-why-theres-after-the-tag-name
@@ -88,19 +90,21 @@ module Gitlab
         end
       end
 
-      def list_remote_tags(remote, env:)
-        tag_list, exit_code, error = nil
-        cmd = %W[#{Gitlab.config.git.bin_path} --git-dir=#{path} ls-remote --tags #{remote}]
+      def list_remote_refs(remote, env:)
+        ref_list, exit_code, error = nil
+
+        # List heads and tags, ignoring stuff like `refs/merge-requests` and `refs/pull`
+        cmd = %W[#{Gitlab.config.git.bin_path} --git-dir=#{path} ls-remote --heads --tags #{remote}]
 
         Open3.popen3(env, *cmd) do |_stdin, stdout, stderr, wait_thr|
-          tag_list  = stdout.read
+          ref_list  = stdout.read
           error     = stderr.read
           exit_code = wait_thr.value.exitstatus
         end
 
         raise RemoteError, error unless exit_code.zero?
 
-        tag_list.split("\n")
+        ref_list.each_line(chomp: true)
       end
     end
   end
