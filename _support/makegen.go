c1a96128366f104a4d6645b7832e92f565939da0
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-05-14 14:09:51 +0200

    makegen: Avoid hardcoding number of make jobs when building git
    We currently hardcode the number of jobs to "8", which may be optimal on
    some platforms, but definitely not on most. Some systems with a smaller
    amount of CPU cores may thus be overloaded while others with a lot more
    cores will be underloaded.
    
    Let's improve this by using the number of cores available to the system,
    plus one.

--- _support/makegen.go
+++ _support/makegen.go
@@ -369,9 +369,9 @@ func (gm *gitalyMake) GitSourceDir() string { return filepath.Join(gm.BuildDir()
 
 func (gm *gitalyMake) GitBuildOptions() string {
 	buildOptions := []string{
-		"-j 8",             // use multiple parallele jobs
-		"DEVELOPER=1",      // activate developer checks
-		"CFLAGS='-O0 -g3'", // make it easy to debug in case of crashes
+		fmt.Sprintf("-j%v", runtime.NumCPU() + 1), // use multiple parallele jobs
+		"DEVELOPER=1",                             // activate developer checks
+		"CFLAGS='-O0 -g3'",                        // make it easy to debug in case of crashes
 		"NO_PERL=YesPlease",
 		"NO_EXPAT=YesPlease",
 		"NO_TCLTK=YesPlease",
