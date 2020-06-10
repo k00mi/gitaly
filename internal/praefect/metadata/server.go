8abc867b19575a01da6ca9939c7e1ad4fd8d44b6
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-06-10 10:56:51 +0200

    praefect: Adjust Praefect server address based on peer info
    Currently, Praefect will inject either its local Unix socket path or its
    listen address into the context, based on which one is set up in its
    configuration. There's multiple problems with this, though:
    
        - First, it could be that both addresses are required based on which
          Gitaly node it reaches out to. E.g. if one Gitaly node is running
          on the same machine, it may use a Unix socket instead of a TCP
          connection. In that case, we should use Praefect's Unix socket to
          reach back out to it. But for another node, we may want to use a
          TCP socket instead as it's hosted on a different machine. So
          injecting either Unix socket or TCP address needs to depend on the
          node we're reaching out to.
    
        - Second, if Praefect's configured to only have a wildcard listen
          address, we'd currently inject "0.0.0.0:$PORT" into the context.
          That's not really helpful at all to Gitaly and doesn't help much
          in connecting to Praefect.
    
    In order to fix both issues, we now start injecting both Unix socket
    path as well as the listen address into the context. Gitaly will then
    extract peer information from the incoming Praefect connection and
    choose the correct one based on whether it's a Unix or TCP connection.
    Furthermore, in case of a TCP connection, Gitaly nodes will now replace
    the host part of the injected listen address with the peer address, only
    keeping the port information intact.

--- internal/praefect/metadata/server.go
+++ internal/praefect/metadata/server.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"strings"
 
 	gitalyauth "gitlab.com/gitlab-org/gitaly/auth"
@@ -13,6 +14,7 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/config"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 )
 
 const (
@@ -32,24 +34,20 @@ var (
 
 // PraefectServer stores parameters required to connect to a Praefect server
 type PraefectServer struct {
-	// Address is the address of the Praefect server
-	Address string `json:"address"`
+	// ListenAddr is the TCP listen address of the Praefect server
+	ListenAddr string `json:"listen_addr"`
+	// SocketPath is the Unix socket path of the Praefect server
+	SocketPath string `json:"socket_path"`
 	// Token is the token required to authenticate with the Praefect server
 	Token string `json:"token"`
 }
 
 // InjectPraefectServer injects Praefect connection metadata into an incoming context
 func InjectPraefectServer(ctx context.Context, conf config.Config) (context.Context, error) {
-	var address string
-	if conf.ListenAddr != "" {
-		address = conf.ListenAddr
-	} else if conf.SocketPath != "" {
-		address = "unix://" + conf.SocketPath
-	}
-
 	praefectServer := PraefectServer{
-		Address: address,
-		Token:   conf.Auth.Token,
+		ListenAddr: strings.TrimPrefix(conf.ListenAddr, "tcp://"),
+		SocketPath: strings.TrimPrefix(conf.SocketPath, "unix://"),
+		Token:      conf.Auth.Token,
 	}
 
 	marshalled, err := json.Marshal(praefectServer)
@@ -66,6 +64,40 @@ func InjectPraefectServer(ctx context.Context, conf config.Config) (context.Cont
 	md.Set(PraefectMetadataKey, base64.StdEncoding.EncodeToString(marshalled))
 
 	return metadata.NewIncomingContext(ctx, md), nil
+}
+
+// Resolve Praefect address based on its peer information. Depending on how
+// Praefect reached out to us, we'll adjust the PraefectServer to contain
+// either its Unix or TCP address.
+func (p *PraefectServer) resolvePraefectAddress(peer *peer.Peer) error {
+	switch addr := peer.Addr.(type) {
+	case *net.UnixAddr:
+		if p.SocketPath == "" {
+			return errors.New("resolvePraefectAddress: got Unix peer but no socket path")
+		}
+
+		p.ListenAddr = ""
+		return nil
+	case *net.TCPAddr:
+		if p.ListenAddr == "" {
+			return errors.New("resolvePraefectAddress: got TCP peer but no listen address")
+		}
+
+		// We need to replace Praefect's IP address with the peer's
+		// address as the value we have is from Praefect's configuration,
+		// which may be a wildcard IP address ("0.0.0.0").
+		_, port, err := net.SplitHostPort(p.ListenAddr)
+		if err != nil {
+			return fmt.Errorf("resolvePraefectAddress: could not resolve address %q: %w", p.ListenAddr, err)
+		}
+
+		p.ListenAddr = net.JoinHostPort(addr.IP.String(), port)
+		p.SocketPath = ""
+
+		return nil
+	default:
+		return fmt.Errorf("resolvePraefectAddress: unknown peer address scheme: %s", peer.Addr.Network())
+	}
 }
 
 // PraefectFromContext extracts `PraefectServer` from an incoming context. In
@@ -89,6 +121,15 @@ func PraefectFromContext(ctx context.Context) (*PraefectServer, error) {
 	var praefect PraefectServer
 	if err := json.Unmarshal(decoded, &praefect); err != nil {
 		return nil, fmt.Errorf("PraefectFromContext: %w", err)
+	}
+
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("PraefectFromContext: could not get peer")
+	}
+
+	if err := praefect.resolvePraefectAddress(peer); err != nil {
+		return nil, err
 	}
 
 	return &praefect, nil
@@ -124,7 +165,7 @@ func PraefectFromEnv(envvars []string) (*PraefectServer, error) {
 }
 
 // Env encodes the `PraefectServer` and returns an environment variable.
-func (p PraefectServer) Env() (string, error) {
+func (p *PraefectServer) Env() (string, error) {
 	marshalled, err := json.Marshal(p)
 	if err != nil {
 		return "", err
@@ -134,8 +175,20 @@ func (p PraefectServer) Env() (string, error) {
 	return fmt.Sprintf("%s=%s", PraefectEnvKey, encoded), nil
 }
 
+func (p *PraefectServer) Address() (string, error) {
+	if p.SocketPath != "" {
+		return "unix://" + p.SocketPath, nil
+	}
+
+	if p.ListenAddr != "" {
+		return "tcp://" + p.ListenAddr, nil
+	}
+
+	return "", errors.New("no address configured")
+}
+
 // Dial will try to connect to the given Praefect server
-func (p PraefectServer) Dial(ctx context.Context) (*grpc.ClientConn, error) {
+func (p *PraefectServer) Dial(ctx context.Context) (*grpc.ClientConn, error) {
 	opts := []grpc.DialOption{
 		grpc.WithBlock(),
 	}
@@ -143,5 +196,10 @@ func (p PraefectServer) Dial(ctx context.Context) (*grpc.ClientConn, error) {
 		opts = append(opts, grpc.WithPerRPCCredentials(gitalyauth.RPCCredentialsV2(p.Token)))
 	}
 
-	return client.DialContext(ctx, p.Address, opts)
+	address, err := p.Address()
+	if err != nil {
+		return nil, err
+	}
+
+	return client.DialContext(ctx, address, opts)
 }
--- internal/service/hook/pre_receive.go
+++ internal/service/hook/pre_receive.go
@@ -55,8 +55,13 @@ func gitlabShellHook(hookName string) string {
 }
 
 func (s *server) getPraefectConn(ctx context.Context, server *metadata.PraefectServer) (*grpc.ClientConn, error) {
+	address, err := server.Address()
+	if err != nil {
+		return nil, err
+	}
+
 	s.mutex.RLock()
-	conn, ok := s.praefectConnPool[server.Address]
+	conn, ok := s.praefectConnPool[address]
 	s.mutex.RUnlock()
 
 	if ok {
@@ -66,7 +71,7 @@ func (s *server) getPraefectConn(ctx context.Context, server *metadata.PraefectS
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	conn, ok = s.praefectConnPool[server.Address]
+	conn, ok = s.praefectConnPool[address]
 	if !ok {
 		var err error
 		conn, err = server.Dial(ctx)
@@ -74,7 +79,7 @@ func (s *server) getPraefectConn(ctx context.Context, server *metadata.PraefectS
 			return nil, err
 		}
 
-		s.praefectConnPool[server.Address] = conn
+		s.praefectConnPool[address] = conn
 	}
 
 	return conn, nil
