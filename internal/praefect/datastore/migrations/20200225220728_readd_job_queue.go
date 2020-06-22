bba4e24fe830209de3582b43343a900e5556fa62
Stan Hu <stanhu@gmail.com>
2020-06-22 06:56:43 -0700

    Ensure Praefect replication queue tables exist
    https://gitlab.com/gitlab-org/gitaly/-/merge_requests/1865 introduced
    gitaly_replication_* tables on 2020-03-10, but
    https://gitlab.com/gitlab-org/gitaly/-/merge_requests/1948 dropped the
    gitaly_ prefix on 2020-03-26.
    
    As a result, any instance that migrated Praefect between those two dates
    would have an inconsistent state of the Praefect database. The only way
    to recover would be to drop the database entirely. To avoid that, we
    readd the replication queue tables if they don't exist to avoid
    migration errors.
    
    Closes https://gitlab.com/gitlab-org/gitaly/-/issues/2896

---
+++ internal/praefect/datastore/migrations/20200225220728_readd_job_queue.go
@@ -0,0 +1,45 @@
+package migrations
+
+import migrate "github.com/rubenv/sql-migrate"
+
+func init() {
+	m := &migrate.Migration{
+		Id: "20200225220728_readd_job_queue",
+		Up: []string{
+			`DO $$ BEGIN
+			   CREATE TYPE REPLICATION_JOB_STATE AS ENUM('ready', 'in_progress', 'completed', 'cancelled', 'failed', 'dead');
+			 EXCEPTION
+			   WHEN duplicate_object THEN null;
+			 END $$
+			`,
+			`CREATE TABLE IF NOT EXISTS replication_queue_lock (
+				id TEXT PRIMARY KEY
+				, acquired BOOLEAN NOT NULL DEFAULT FALSE
+			)`,
+			`CREATE TABLE IF NOT EXISTS replication_queue (
+				id BIGSERIAL PRIMARY KEY
+				, state REPLICATION_JOB_STATE NOT NULL DEFAULT 'ready'
+				, created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
+				, updated_at TIMESTAMP WITHOUT TIME ZONE
+				, attempt INTEGER NOT NULL DEFAULT 3
+				, lock_id TEXT
+				, job JSONB
+				, meta JSONB
+			)`,
+			`CREATE TABLE IF NOT EXISTS replication_queue_job_lock (
+				job_id BIGINT REFERENCES replication_queue(id)
+				, lock_id TEXT REFERENCES replication_queue_lock(id)
+				, triggered_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
+				, CONSTRAINT replication_queue_job_lock_pk PRIMARY KEY (job_id, lock_id)
+			)`,
+		},
+		Down: []string{
+			`DROP TABLE IF EXISTS replication_queue_job_lock CASCADE`,
+			`DROP TABLE IF EXISTS replication_queue CASCADE`,
+			`DROP TABLE IF EXISTS replication_queue_lock CASCADE`,
+			`DROP TYPE IF EXISTS REPLICATION_JOB_STATE`,
+		},
+	}
+
+	allMigrations = append(allMigrations, m)
+}
