2d3fc8063ff8525c5a0e0d74f4a1a474fc6171ed
Pavlo Strokov <pstrokov@gitlab.com>
2020-09-03 10:57:45 +0300

    Replication job acknowledge removes 'completed' and 'dead' events.
    Removal of data in a single transactional statement instead of
    batching each in separate transaction as there are not much to
    remove.
    
    Closes: https://gitlab.com/gitlab-org/gitaly/-/issues/2941

--- internal/praefect/datastore/migrations/20200810055650_replication_queue_cleanup.go
+++ internal/praefect/datastore/migrations/20200810055650_replication_queue_cleanup.go
@@ -6,38 +6,11 @@ func init() {
 	m := &migrate.Migration{
 		Id: "20200810055650_replication_queue_cleanup",
 		Up: []string{
-			`CREATE TEMP TABLE ids(id BIGINT)`,
-
-			`-- +migrate StatementBegin
-			DO $$DECLARE
-			    found_amount BIGINT;
-			BEGIN
-			    LOOP
-				TRUNCATE TABLE ids;
-
-				INSERT INTO ids
-				SELECT id
-				FROM replication_queue
-				WHERE state = ANY (ARRAY['dead'::REPLICATION_JOB_STATE, 'completed'::REPLICATION_JOB_STATE])
-				LIMIT 100000;
-
-				SELECT COUNT(*) INTO found_amount FROM ids;
-
-				IF found_amount > 0 THEN
-				    DELETE FROM replication_queue WHERE id IN (SELECT id FROM ids);
-				    COMMIT;
-				ELSE
-				    RETURN;
-				END IF;
-
-			    END LOOP;
-			END$$;
-			-- +migrate StatementEnd`,
-
-			`DROP TABLE ids`,
+			`DELETE FROM replication_queue WHERE state = ANY (ARRAY['dead'::REPLICATION_JOB_STATE, 'completed'::REPLICATION_JOB_STATE])`,
 		},
-		Down:                 []string{},
-		DisableTransactionUp: true,
+		Down: []string{
+			// there is no way to restore deleted data so far
+		},
 	}
 
 	allMigrations = append(allMigrations, m)
