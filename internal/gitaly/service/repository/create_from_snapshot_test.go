307702049c39a85d9660b5cf17e8d5b423fbcd74
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-09-11 09:04:43 +0200

    archive: Move into gitaly package

--- internal/gitaly/service/repository/create_from_snapshot_test.go
+++ internal/gitaly/service/repository/create_from_snapshot_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitaly/internal/archive"
+	"gitlab.com/gitlab-org/gitaly/internal/gitaly/archive"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 	"google.golang.org/grpc/codes"
