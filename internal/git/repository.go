67676fc9e488c335b09a0d7af01c90d9e5f6c938
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-09-11 08:03:41 +0200

    git: Improve error handling for git.ContainsRef
    We're currently checking for exit errors when creating the `git
    rev-parse` command, not when actually executing it. Even though the code
    works correctly, it's the wrong way to have that check, so let's move it
    after `cmd.Wait()` instead.

--- internal/git/repository.go
+++ internal/git/repository.go
@@ -67,13 +67,17 @@ func (repo *localRepository) ContainsRef(ctx context.Context, ref string) (bool,
 		Args:  []string{ref},
 	})
 	if err != nil {
+		return false, err
+	}
+
+	if err := cmd.Wait(); err != nil {
 		if _, ok := err.(*exec.ExitError); ok {
 			return false, nil
 		}
 		return false, err
 	}
 
-	return cmd.Wait() == nil, nil
+	return true, nil
 }
 
 func (repo *localRepository) GetReference(ctx context.Context, ref string) (Reference, error) {
