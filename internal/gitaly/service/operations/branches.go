c3b32722643bc6eded57544da8785a7780ced477
Christian Couder <chriscool@tuxfamily.org>
2020-09-29 15:51:12 +0200

    Initial go implementation of UserDeleteBranch

--- internal/gitaly/service/operations/branches.go
+++ internal/gitaly/service/operations/branches.go
@@ -2,8 +2,13 @@ package operations
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/rubyserver"
+	"gitlab.com/gitlab-org/gitaly/internal/helper"
+	"gitlab.com/gitlab-org/gitaly/internal/metadata/featureflag"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -46,6 +51,42 @@ func (s *server) UserDeleteBranch(ctx context.Context, req *gitalypb.UserDeleteB
 		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty user)")
 	}
 
+	if featureflag.IsDisabled(ctx, featureflag.GoUserDeleteBranch) {
+		return s.UserDeleteBranchRuby(ctx, req)
+	}
+
+	// Implement UserDeleteBranch in Go
+
+	revision, err := parseRevision(ctx, req.Repository, string(req.BranchName))
+	if err != nil {
+		return nil, helper.ErrPreconditionFailed(err)
+	}
+
+	branch := fmt.Sprintf("refs/heads/%s", req.BranchName)
+
+	if err := s.updateReferenceWithHooks(ctx, req.Repository, req.User, branch, git.NullSHA, revision); err != nil {
+		var preReceiveError preReceiveError
+		if errors.As(err, &preReceiveError) {
+			return &gitalypb.UserDeleteBranchResponse{
+				PreReceiveError: preReceiveError.message,
+			}, nil
+		}
+
+		var updateRefError updateRefError
+		if errors.As(err, &updateRefError) {
+			// When an error happens updating the reference, e.g. because of a race
+			// with another update, then Ruby code didn't send an error but just an
+			// empty response.
+			return &gitalypb.UserDeleteBranchResponse{}, nil
+		}
+
+		return nil, err
+	}
+
+	return &gitalypb.UserDeleteBranchResponse{}, nil
+}
+
+func (s *server) UserDeleteBranchRuby(ctx context.Context, req *gitalypb.UserDeleteBranchRequest) (*gitalypb.UserDeleteBranchResponse, error) {
 	client, err := s.ruby.OperationServiceClient(ctx)
 	if err != nil {
 		return nil, err
