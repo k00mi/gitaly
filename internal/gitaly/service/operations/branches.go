c1e3ccca64ea4622d7d5117ba17e7de1f0d66312
Christian Couder <chriscool@tuxfamily.org>
2020-09-30 02:21:04 +0200

    Initial go implementation of UserCreateBranch

--- internal/gitaly/service/operations/branches.go
+++ internal/gitaly/service/operations/branches.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"gitlab.com/gitlab-org/gitaly/internal/git"
+	"gitlab.com/gitlab-org/gitaly/internal/git/log"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/rubyserver"
 	"gitlab.com/gitlab-org/gitaly/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/internal/metadata/featureflag"
@@ -15,6 +16,64 @@ import (
 )
 
 func (s *server) UserCreateBranch(ctx context.Context, req *gitalypb.UserCreateBranchRequest) (*gitalypb.UserCreateBranchResponse, error) {
+	if featureflag.IsDisabled(ctx, featureflag.GoUserCreateBranch) {
+		return s.UserCreateBranchRuby(ctx, req)
+	}
+
+	// Implement UserCreateBranch in Go
+
+	if len(req.BranchName) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty branch name)")
+	}
+
+	if req.User == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty user)")
+	}
+
+	if len(req.StartPoint) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty starting point)")
+	}
+
+	startPointCommit, err := log.GetCommit(ctx, req.Repository, string(req.StartPoint))
+	if err != nil {
+		return nil, helper.ErrPreconditionFailed(err)
+	}
+
+	_, err = parseRevision(ctx, req.Repository, string(req.BranchName))
+	if err == nil {
+		return nil, status.Errorf(codes.FailedPrecondition, "Bad Request (branch exists)")
+	}
+
+	branch := fmt.Sprintf("refs/heads/%s", req.BranchName)
+
+	if err := s.updateReferenceWithHooks(ctx, req.Repository, req.User, branch, string(req.StartPoint), git.NullSHA); err != nil {
+		var preReceiveError preReceiveError
+		if errors.As(err, &preReceiveError) {
+			return &gitalypb.UserCreateBranchResponse{
+				PreReceiveError: preReceiveError.message,
+			}, nil
+		}
+
+		var updateRefError updateRefError
+		if errors.As(err, &updateRefError) {
+			// When an error happens updating the reference, e.g. because of a race
+			// with another update, then Ruby code didn't send an error but just an
+			// empty response.
+			return &gitalypb.UserCreateBranchResponse{}, nil
+		}
+
+		return nil, err
+	}
+
+	return &gitalypb.UserCreateBranchResponse{
+		Branch: &gitalypb.Branch{
+			Name:         req.BranchName,
+			TargetCommit: startPointCommit,
+		},
+	}, nil
+}
+
+func (s *server) UserCreateBranchRuby(ctx context.Context, req *gitalypb.UserCreateBranchRequest) (*gitalypb.UserCreateBranchResponse, error) {
 	client, err := s.ruby.OperationServiceClient(ctx)
 	if err != nil {
 		return nil, err
