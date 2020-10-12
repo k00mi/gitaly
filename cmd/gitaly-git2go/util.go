831cb43987a98e817ce88d5c0b26ad345c07418b
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-10-12 10:46:39 +0200

    git2go: Improve error messages when looking up commits
    The current error messages when lookup of commits fails doesn't give any
    hint what reference was actually looked up, which makes it harder than
    necessary to understand the context.
    
    Fix this by adding the reference name to those error messages.

--- cmd/gitaly-git2go/util.go
+++ cmd/gitaly-git2go/util.go
@@ -11,17 +11,17 @@ import (
 func lookupCommit(repo *git.Repository, ref string) (*git.Commit, error) {
 	object, err := repo.RevparseSingle(ref)
 	if err != nil {
-		return nil, fmt.Errorf("could not lookup reference: %w", err)
+		return nil, fmt.Errorf("could not lookup reference %q: %w", ref, err)
 	}
 
 	peeled, err := object.Peel(git.ObjectCommit)
 	if err != nil {
-		return nil, fmt.Errorf("could not peel reference: %w", err)
+		return nil, fmt.Errorf("could not peel reference %q: %w", ref, err)
 	}
 
 	commit, err := peeled.AsCommit()
 	if err != nil {
-		return nil, fmt.Errorf("could not cast to commit: %w", err)
+		return nil, fmt.Errorf("reference %q is not a commit: %w", ref, err)
 	}
 
 	return commit, nil
