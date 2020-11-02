2cb6b2251c2a7524537c0c796ec8be24034b6b1f
Sami Hiltunen <shiltunen@gitlab.com>
2020-11-02 11:03:01 +0100

    remove concept of primary from replicator
    ReplMgr doesn't need to know which node is the primary. Due to
    depedning directly on NodeManager, the current interface relies on
    the idea of having a primary for a virtual storage. This MR removes
    that expectation by decoupling ReplMgr from NodeManager. This is
    necessary to hook in the per repository elector as there will be
    no primary for a virtual storage anymore.
    
    The interface is replaced with a HealthChecker that simply returns
    a list of healthy storages in each virtual storage which are then
    used to filter the static set of connections injected as well.
    
    The interface matches what the individual components that build up
    the per repository elector provide.

--- internal/praefect/router.go
+++ internal/praefect/router.go
@@ -13,8 +13,32 @@ import (
 type Node struct {
 	// Storage is the name of the storage node.
 	Storage string
+	// Address is the address of the node.
+	Address string
+	// Token is the authentication token of the node.
+	Token string
 	// Connection is a gRPC connection to the storage node.
 	Connection *grpc.ClientConn
+}
+
+// NodeSet contains nodes by their virtual storage and storage names.
+type NodeSet map[string]map[string]Node
+
+// NodeSetFromNodeManager converts connections set up by the node manager
+// in to a NodeSet. This is a temporary adapter required due to cyclic
+// imports between the praefect and nodes packages.
+func NodeSetFromNodeManager(mgr nodes.Manager) NodeSet {
+	nodes := mgr.Nodes()
+
+	set := make(NodeSet, len(nodes))
+	for virtualStorage, nodes := range nodes {
+		set[virtualStorage] = make(map[string]Node, len(nodes))
+		for _, node := range nodes {
+			set[virtualStorage][node.GetStorage()] = toNode(node)
+		}
+	}
+
+	return set
 }
 
 // StorageMutatorRoute describes how to route a storage scoped mutator call.
@@ -58,6 +82,8 @@ type nodeManagerRouter struct {
 func toNode(node nodes.Node) Node {
 	return Node{
 		Storage:    node.GetStorage(),
+		Address:    node.GetAddress(),
+		Token:      node.GetToken(),
 		Connection: node.GetConnection(),
 	}
 }
