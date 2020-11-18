7d88b02db6864d7ae5b7cfe319a2f183bdcca389
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-11-18 16:30:27 +0100

    tempdir: Move "clean" root directory into global test directory
    Inside our "tempdir" tests, we're using a "clean" directory to store
    temporary storages, which is currently created in our "testdata". This
    commit instead moves it into our global temporary test directory.

--- internal/tempdir/testhelper_test.go
+++ internal/tempdir/testhelper_test.go
@@ -1,11 +1,16 @@
 package tempdir
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 )
+
+var cleanRoot string
 
 func TestMain(m *testing.M) {
 	os.Exit(testMain(m))
@@ -15,5 +20,15 @@ func testMain(m *testing.M) int {
 	defer testhelper.MustHaveNoChildProcess()
 	cleanup := testhelper.Configure()
 	defer cleanup()
+
+	tempDir, err := ioutil.TempDir("", "gitaly-tests")
+	if err != nil {
+		log.Error(err)
+		return 1
+	}
+	defer os.RemoveAll(tempDir)
+
+	cleanRoot = filepath.Join(tempDir, tmpRootPrefix)
+
 	return m.Run()
 }
