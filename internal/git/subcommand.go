908f7d145e2b4fbef76bae889c0fc377fc627584
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-11-24 13:03:37 +0100

    git: Convert subcommands to use flags
    During the ongoing work to globally enable the reference-transaction
    hook, we have grown multiple maps which track different Git subcommands
    in order to categorize by different characteristics. Right now those
    include whether the command is read-only or never changes references,
    but there are additional characteristics which are of interest.
    
    Let's refactor the two maps we have into a single map which holds all
    subcommands, where each subcommand has a bitfield of flags. This allows
    us to more easily add additional characteristics without having to
    create new maps for each of them.

--- internal/git/subcommand.go
+++ internal/git/subcommand.go
@@ -1,67 +1,41 @@
 package git
 
-// Curated list of Git command names for special git.SafeCmd validation logic
 const (
-	scCatFile        = "cat-file"
-	scLog            = "log"
-	scForEachRef     = "for-each-ref"
-	scRevParse       = "rev-parse"
-	scCountObjects   = "count-objects"
-	scConfig         = "config"
-	scMultiPackIndex = "multi-pack-index"
-	scRepack         = "repack"
-	scDiff           = "diff"
-	scDiffTree       = "diff-tree"
-	scPackRefs       = "pack-refs"
-	scMergeBase      = "merge-base"
-	scHashObject     = "hash-object"
-	scShowRef        = "show-ref"
-	scUploadPack     = "upload-pack"
-	scUploadArchive  = "upload-archive"
-	scBlame          = "blame"
-	scLsTree         = "ls-tree"
-	scRevList        = "rev-list"
-	scLsRemote       = "ls-remote"
-	scFsck           = "fsck"
-	scGrep           = "grep"
-	scBundle         = "bundle"
-	scArchive        = "archive"
-	scFormatPatch    = "format-patch"
-	scInit           = "init"
+	// scReadOnly denotes a read-only command
+	scReadOnly = 1 << iota
+	// scNoRefUpdates denotes a command which will never update refs
+	scNoRefUpdates
 )
 
-var knownReadOnlyCmds = map[string]struct{}{
-	scCatFile:       struct{}{},
-	scLog:           struct{}{},
-	scForEachRef:    struct{}{},
-	scRevParse:      struct{}{},
-	scCountObjects:  struct{}{},
-	scDiff:          struct{}{},
-	scDiffTree:      struct{}{},
-	scMergeBase:     struct{}{},
-	scShowRef:       struct{}{},
-	scUploadPack:    struct{}{},
-	scUploadArchive: struct{}{},
-	scBlame:         struct{}{},
-	scLsTree:        struct{}{},
-	scRevList:       struct{}{},
-	scLsRemote:      struct{}{},
-	scFsck:          struct{}{},
-	scGrep:          struct{}{},
-	scBundle:        struct{}{},
-	scArchive:       struct{}{},
-	scFormatPatch:   struct{}{},
-}
-
-// knownNoRefUpdates indicates all repo mutating commands where it is known
-// whether references are never updated
-var knownNoRefUpdates = map[string]struct{}{
-	scConfig:         struct{}{},
-	scMultiPackIndex: struct{}{},
-	scRepack:         struct{}{},
-	scPackRefs:       struct{}{},
-	scHashObject:     struct{}{},
-	scInit:           struct{}{},
+// subcommands is a curated list of Git command names for special git.SafeCmd
+// validation logic
+var subcommands = map[string]uint{
+	"archive":          scReadOnly,
+	"blame":            scReadOnly,
+	"bundle":           scReadOnly,
+	"cat-file":         scReadOnly,
+	"config":           scNoRefUpdates,
+	"count-objects":    scReadOnly,
+	"diff":             scReadOnly,
+	"diff-tree":        scReadOnly,
+	"for-each-ref":     scReadOnly,
+	"format-patch":     scReadOnly,
+	"fsck":             scReadOnly,
+	"grep":             scReadOnly,
+	"hash-object":      scNoRefUpdates,
+	"init":             scNoRefUpdates,
+	"log":              scReadOnly,
+	"ls-remote":        scReadOnly,
+	"ls-tree":          scReadOnly,
+	"merge-base":       scReadOnly,
+	"multi-pack-index": scNoRefUpdates,
+	"pack-refs":        scNoRefUpdates,
+	"repack":           scNoRefUpdates,
+	"rev-list":         scReadOnly,
+	"rev-parse":        scReadOnly,
+	"show-ref":         scReadOnly,
+	"upload-archive":   scReadOnly,
+	"upload-pack":      scReadOnly,
 }
 
 // mayUpdateRef indicates if a subcommand is known to update references.
@@ -70,11 +44,9 @@ var knownNoRefUpdates = map[string]struct{}{
 // refs are updated. When unknown, true is returned to err on the side of
 // caution.
 func mayUpdateRef(subcmd string) bool {
-	if _, ok := knownReadOnlyCmds[subcmd]; ok {
-		return false
+	flags, ok := subcommands[subcmd]
+	if !ok {
+		return true
 	}
-	if _, ok := knownNoRefUpdates[subcmd]; ok {
-		return false
-	}
-	return true
+	return flags&(scReadOnly|scNoRefUpdates) == 0
 }
