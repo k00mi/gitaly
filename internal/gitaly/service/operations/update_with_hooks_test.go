6ace6f42ca1b3e24ec14d5793cb3cab55f1a03c9
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-11-26 12:06:12 +0100

    operations: Add a bunch of tests to verify hook execution
    This commit adds a bunch of tests to verify updating references with
    hooks works as expected.

---
+++ internal/gitaly/service/operations/update_with_hooks_test.go
@@ -0,0 +1,276 @@
+package operations
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/internal/git"
+	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/internal/gitaly/hook"
+	hookservice "gitlab.com/gitlab-org/gitaly/internal/gitaly/service/hook"
+	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+)
+
+type mockHookManager struct {
+	t                    *testing.T
+	preReceive           func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error
+	postReceive          func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, pushOptions, env []string, stdin io.Reader, stdout, stderr io.Writer) error
+	update               func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error
+	referenceTransaction func(t *testing.T, ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error
+}
+
+func (m *mockHookManager) PreReceiveHook(ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	return m.preReceive(m.t, ctx, repo, env, stdin, stdout, stderr)
+}
+
+func (m *mockHookManager) PostReceiveHook(ctx context.Context, repo *gitalypb.Repository, pushOptions, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	return m.postReceive(m.t, ctx, repo, pushOptions, env, stdin, stdout, stderr)
+}
+
+func (m *mockHookManager) UpdateHook(ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error {
+	return m.update(m.t, ctx, repo, ref, oldValue, newValue, env, stdout, stderr)
+}
+
+func (m *mockHookManager) ReferenceTransactionHook(ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error {
+	return m.referenceTransaction(m.t, ctx, state, env, stdin)
+}
+
+func TestUpdateReferenceWithHooks_invalidParameters(t *testing.T) {
+	repo, _, cleanup := testhelper.NewTestRepo(t)
+	defer cleanup()
+
+	user := &gitalypb.User{
+		GlId:       "1234",
+		GlUsername: "Username",
+		Name:       []byte("Name"),
+		Email:      []byte("mail@example.com"),
+	}
+
+	revA, revB := strings.Repeat("a", 40), strings.Repeat("b", 40)
+
+	server := NewServer(config.Config, nil, &mockHookManager{}, nil, nil)
+
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	testCases := []struct {
+		desc                string
+		ref, newRev, oldRev string
+		expectedErr         string
+	}{
+		{
+			desc:        "missing reference",
+			oldRev:      revA,
+			newRev:      revB,
+			expectedErr: "got no reference",
+		},
+		{
+			desc:        "missing old rev",
+			ref:         "refs/heads/master",
+			newRev:      revB,
+			expectedErr: "got invalid old value",
+		},
+		{
+			desc:        "missing new rev",
+			ref:         "refs/heads/master",
+			oldRev:      revB,
+			expectedErr: "got invalid new value",
+		},
+		{
+			desc:        "invalid old rev",
+			ref:         "refs/heads/master",
+			newRev:      revA,
+			oldRev:      "foobar",
+			expectedErr: "got invalid old value",
+		},
+		{
+			desc:        "invalid new rev",
+			ref:         "refs/heads/master",
+			newRev:      "foobar",
+			oldRev:      revB,
+			expectedErr: "got invalid new value",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := server.updateReferenceWithHooks(ctx, repo, user, tc.ref, tc.newRev, tc.oldRev)
+			require.Contains(t, err.Error(), tc.expectedErr)
+		})
+	}
+}
+
+func TestUpdateReferenceWithHooks(t *testing.T) {
+	server := testhelper.NewServer(t, nil, nil, testhelper.WithInternalSocket(config.Config))
+	defer server.Stop()
+
+	// We need to set up a separate "real" hook service here, as it will be used in
+	// git-update-ref(1) spawned by `updateRefWithHooks()`
+	gitalypb.RegisterHookServiceServer(server.GrpcServer(), hookservice.NewServer(config.Config, hook.NewManager(hook.GitlabAPIStub, config.Config)))
+	require.NoError(t, server.Start())
+
+	user := &gitalypb.User{
+		GlId:       "1234",
+		GlUsername: "Username",
+		Name:       []byte("Name"),
+		Email:      []byte("mail@example.com"),
+	}
+
+	oldRev := "1e292f8fedd741b75372e19097c76d327140c312"
+
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	expectedEnv := []string{
+		"GITALY_BIN_DIR=" + config.Config.BinDir,
+		"GITALY_SOCKET=" + config.Config.GitalyInternalSocketPath(),
+		"GITALY_TOKEN=the-secret-token",
+		"GL_ID=1234",
+		"GL_PROJECT_PATH=gitlab-org/gitlab-test",
+		"GL_PROTOCOL=web",
+		"GL_REPOSITORY=project-1",
+		"GL_USERNAME=Username",
+	}
+
+	testCases := []struct {
+		desc                 string
+		preReceive           func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error
+		postReceive          func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, pushOptions, env []string, stdin io.Reader, stdout, stderr io.Writer) error
+		update               func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error
+		referenceTransaction func(t *testing.T, ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error
+		expectedErr          string
+		expectedRefDeletion  bool
+	}{
+		{
+			desc: "successful update",
+			preReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				changes, err := ioutil.ReadAll(stdin)
+				require.NoError(t, err)
+				require.Equal(t, fmt.Sprintf("%s %s refs/heads/master\n", oldRev, git.NullSHA), string(changes))
+				require.Subset(t, env, expectedEnv)
+				return nil
+			},
+			update: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error {
+				require.Equal(t, "refs/heads/master", ref)
+				require.Equal(t, oldRev, oldValue)
+				require.Equal(t, newValue, git.NullSHA)
+				require.Subset(t, env, expectedEnv)
+				return nil
+			},
+			postReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, pushOptions, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				changes, err := ioutil.ReadAll(stdin)
+				require.NoError(t, err)
+				require.Equal(t, fmt.Sprintf("%s %s refs/heads/master\n", oldRev, git.NullSHA), string(changes))
+				require.Subset(t, env, expectedEnv)
+				require.Empty(t, pushOptions)
+				return nil
+			},
+			referenceTransaction: func(t *testing.T, ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error {
+				changes, err := ioutil.ReadAll(stdin)
+				require.NoError(t, err)
+				require.Equal(t, fmt.Sprintf("%s %s refs/heads/master\n", oldRev, git.NullSHA), string(changes))
+				require.Equal(t, state, hook.ReferenceTransactionPrepared)
+				require.Subset(t, env, expectedEnv)
+				return nil
+			},
+			expectedRefDeletion: true,
+		},
+		{
+			desc: "prereceive error",
+			preReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				_, err := io.Copy(stderr, strings.NewReader("prereceive failure"))
+				require.NoError(t, err)
+				return errors.New("ignored")
+			},
+			expectedErr: "prereceive failure",
+		},
+		{
+			desc: "update error",
+			preReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				return nil
+			},
+			update: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error {
+				_, err := io.Copy(stderr, strings.NewReader("update failure"))
+				require.NoError(t, err)
+				return errors.New("ignored")
+			},
+			expectedErr: "update failure",
+		},
+		{
+			desc: "reference-transaction error",
+			preReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				return nil
+			},
+			update: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error {
+				return nil
+			},
+			referenceTransaction: func(t *testing.T, ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error {
+				// The reference-transaction hook doesn't execute any custom hooks,
+				// which is why it currently doesn't have any stdout/stderr.
+				// Instead, errors are directly returned.
+				return errors.New("reference-transaction failure")
+			},
+			expectedErr: "reference-transaction failure",
+		},
+		{
+			desc: "post-receive error",
+			preReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				return nil
+			},
+			update: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, ref, oldValue, newValue string, env []string, stdout, stderr io.Writer) error {
+				return nil
+			},
+			referenceTransaction: func(t *testing.T, ctx context.Context, state hook.ReferenceTransactionState, env []string, stdin io.Reader) error {
+				return nil
+			},
+			postReceive: func(t *testing.T, ctx context.Context, repo *gitalypb.Repository, pushOptions, env []string, stdin io.Reader, stdout, stderr io.Writer) error {
+				_, err := io.Copy(stderr, strings.NewReader("post-receive failure"))
+				require.NoError(t, err)
+				return errors.New("ignored")
+			},
+			expectedErr:         "post-receive failure",
+			expectedRefDeletion: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			hookManager := &mockHookManager{
+				t:                    t,
+				preReceive:           tc.preReceive,
+				postReceive:          tc.postReceive,
+				update:               tc.update,
+				referenceTransaction: tc.referenceTransaction,
+			}
+
+			hookServer := NewServer(config.Config, nil, hookManager, nil, nil)
+
+			repo, _, cleanup := testhelper.NewTestRepo(t)
+			defer cleanup()
+
+			err := hookServer.updateReferenceWithHooks(ctx, repo, user, "refs/heads/master", git.NullSHA, oldRev)
+			if tc.expectedErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Contains(t, err.Error(), tc.expectedErr)
+			}
+
+			if tc.expectedRefDeletion {
+				contained, err := git.NewRepository(repo).ContainsRef(ctx, "refs/heads/master")
+				require.NoError(t, err)
+				require.False(t, contained, "branch should have been deleted")
+			} else {
+				ref, err := git.NewRepository(repo).GetReference(ctx, "refs/heads/master")
+				require.NoError(t, err)
+				require.Equal(t, ref.Target, oldRev)
+			}
+		})
+	}
+}
