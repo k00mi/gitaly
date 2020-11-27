0e68ca3ff3b434bbd650633a1bedf708fa653bf1
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-11-27 14:09:08 +0100

    testhelper: Move test setups into single file
    Our `ConfigureFoo()` functions are used to set up the global test state,
    e.g. by configuring storages, creating the temporary directory or
    compiling required binaries. They're currently a bit scattered though.
    
    Unify them all into a single new file so they're all colocated.

--- internal/testhelper/hook_env.go
+++ internal/testhelper/hook_env.go
@@ -2,14 +2,12 @@ package testhelper
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/gitlab-org/gitaly/internal/git/hooks"
-	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
 )
 
 // CaptureHookEnv creates a bogus 'update' Git hook to sniff out what
@@ -39,19 +37,4 @@ env | grep -e ^GIT -e ^GL_ > ` + hookOutputFile + "\n")
 		cleanup()
 		hooks.Override = oldOverride
 	}
-}
-
-// ConfigureGitalyHooksBinary builds gitaly-hooks command for tests
-func ConfigureGitalyHooksBinary() {
-	if config.Config.BinDir == "" {
-		log.Fatal("config.Config.BinDir must be set")
-	}
-
-	goBuildArgs := []string{
-		"build",
-		"-o",
-		filepath.Join(config.Config.BinDir, "gitaly-hooks"),
-		"gitlab.com/gitlab-org/gitaly/cmd/gitaly-hooks",
-	}
-	MustRunCommand(nil, nil, "go", goBuildArgs...)
 }
