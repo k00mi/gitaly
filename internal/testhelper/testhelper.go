06b7cbadd053da5f4b12ac58e2a56eb51155daa9
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-11-30 15:26:41 +0100

    testhelper: Move object-related helpers into their own file
    This commit moves all Git object-related helpers into their own file to
    make them easier to discover.

--- internal/testhelper/testhelper.go
+++ internal/testhelper/testhelper.go
@@ -1,7 +1,6 @@
 package testhelper
 
 import (
-	"bytes"
 	"context"
 	"crypto/ecdsa"
 	"crypto/elliptic"
@@ -365,57 +364,9 @@ func TempDir(t testing.TB) (string, func()) {
 	return tmpDir, func() { require.NoError(t, os.RemoveAll(tmpDir)) }
 }
 
-// GitObjectMustExist is a test assertion that fails unless the git repo in repoPath contains sha
-func GitObjectMustExist(t testing.TB, repoPath, sha string) {
-	gitObjectExists(t, repoPath, sha, true)
-}
-
-// GitObjectMustNotExist is a test assertion that fails unless the git repo in repoPath contains sha
-func GitObjectMustNotExist(t testing.TB, repoPath, sha string) {
-	gitObjectExists(t, repoPath, sha, false)
-}
-
-func gitObjectExists(t testing.TB, repoPath, sha string, exists bool) {
-	cmd := exec.Command(config.Config.Git.BinPath, "-C", repoPath, "cat-file", "-e", sha)
-	cmd.Env = []string{
-		"GIT_ALLOW_PROTOCOL=", // To prevent partial clone reaching remote repo over SSH
-	}
-
-	if exists {
-		require.NoError(t, cmd.Run(), "checking for object should succeed")
-		return
-	}
-	require.Error(t, cmd.Run(), "checking for object should fail")
-}
-
 // Cleanup functions should be called in a defer statement
 // immediately after they are returned from a test helper
 type Cleanup func()
-
-// GetGitObjectDirSize gets the number of 1k blocks of a git object directory
-func GetGitObjectDirSize(t testing.TB, repoPath string) int64 {
-	return getGitDirSize(t, repoPath, "objects")
-}
-
-// GetGitPackfileDirSize gets the number of 1k blocks of a git object directory
-func GetGitPackfileDirSize(t testing.TB, repoPath string) int64 {
-	return getGitDirSize(t, repoPath, "objects", "pack")
-}
-
-func getGitDirSize(t testing.TB, repoPath string, subdirs ...string) int64 {
-	cmd := exec.Command("du", "-s", "-k", filepath.Join(append([]string{repoPath}, subdirs...)...))
-	output, err := cmd.Output()
-	require.NoError(t, err)
-	if len(output) < 2 {
-		t.Error("invalid output of du -s -k")
-	}
-
-	outputSplit := strings.SplitN(string(output), "\t", 2)
-	blocks, err := strconv.ParseInt(outputSplit[0], 10, 64)
-	require.NoError(t, err)
-
-	return blocks
-}
 
 // WriteExecutable ensures that the parent directory exists, and writes an executable with provided content
 func WriteExecutable(path string, content []byte) (func(), error) {
@@ -427,17 +378,6 @@ func WriteExecutable(path string, content []byte) (func(), error) {
 	return func() {
 		os.RemoveAll(dir)
 	}, ioutil.WriteFile(path, content, 0755)
-}
-
-func WriteBlobs(t testing.TB, testRepoPath string, n int) []string {
-	var blobIDs []string
-	for i := 0; i < n; i++ {
-		var stdin bytes.Buffer
-		stdin.Write([]byte(strconv.Itoa(time.Now().Nanosecond())))
-		blobIDs = append(blobIDs, text.ChompBytes(MustRunCommand(t, &stdin, "git", "-C", testRepoPath, "hash-object", "-w", "--stdin")))
-	}
-
-	return blobIDs
 }
 
 // ModifyEnvironment will change an environment variable and return a func suitable
