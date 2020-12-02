807efa20454da3210f8d1fc59da97816e5635d25
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-12-02 11:03:47 +0100

    git: Implement a new hooks payload structure
    In order to pass information to Git hooks, we need to use environment
    variables as execution of those hooks is controlled by Git and not by
    us. As such, we have over time grown a set of variables which our hooks
    expect to always be present. Unfortunately, the downside is that now we
    have this environment setup all over the place while it is not obvious
    which environment variables are expected to be present where. The result
    is code which is hard to understand which leads to more bugs in our
    already fragile hook code.
    
    This commit is a first step to fix this by introducing a new variable
    "GITALY_HOOKS_PAYLOAD" [1]. This variable is going to hold a serialized
    structure which contains most the parameters required to execute hooks.
    For now, it only contains information about the binary directory,
    internal socket as well as the repository. But in future iterations,
    this will be extended to also contain all of our "GL_" values and
    possibly other values which are required.
    
    This conversion also fixes an ambiguity in environment variables we have
    recently introduced. We nowadays run most of our commands with a hook
    environment which sets up "GITALY_SOCKET" and the likes. But in fact,
    the gitaly-ssh transport helper we use for fetching from other Gitaly
    servers requires these variables to point to the remote Gitaly server.
    As a result, we now have a conflict in intent, which is also going to be
    solved by the new payload.
    
    [1]: https://xkcd.com/927/

--- internal/git/hooks_test.go
+++ internal/git/hooks_test.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/internal/command"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
@@ -75,4 +76,105 @@ func TestWithRefHook(t *testing.T) {
 			}, actualEnvVars)
 		})
 	}
+}
+
+func TestHooksPayload(t *testing.T) {
+	repo, _, cleanup := testhelper.NewTestRepo(t)
+	defer cleanup()
+
+	marshaller := &jsonpb.Marshaler{}
+	marshalledRepo, err := marshaller.MarshalToString(repo)
+	require.NoError(t, err)
+
+	t.Run("envvar has proper name", func(t *testing.T) {
+		env, err := NewHooksPayload(config.Config, repo).Env()
+		require.NoError(t, err)
+		require.True(t, strings.HasPrefix(env, ErrHooksPayloadNotFound+"="))
+	})
+
+	t.Run("roundtrip succeeds", func(t *testing.T) {
+		env, err := NewHooksPayload(config.Config, repo).Env()
+		require.NoError(t, err)
+
+		payload, err := HooksPayloadFromEnv([]string{
+			"UNRELATED=value",
+			env,
+			"ANOTHOR=unrelated-value",
+			ErrHooksPayloadNotFound + "_WITH_SUFFIX=is-ignored",
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, HooksPayload{
+			Repo:           repo,
+			BinDir:         config.Config.BinDir,
+			InternalSocket: config.Config.GitalyInternalSocketPath(),
+		}, payload)
+	})
+
+	t.Run("missing envvar", func(t *testing.T) {
+		_, err := HooksPayloadFromEnv([]string{"OTHER_ENV=foobar"})
+		require.Error(t, err)
+		require.Equal(t, ErrPayloadNotFound, err)
+	})
+
+	t.Run("bogus value", func(t *testing.T) {
+		_, err := HooksPayloadFromEnv([]string{ErrHooksPayloadNotFound + "=foobar"})
+		require.Error(t, err)
+	})
+
+	t.Run("fallback", func(t *testing.T) {
+		payload, err := HooksPayloadFromEnv([]string{
+			"GITALY_BIN_DIR=/bin/dir",
+			"GITALY_SOCKET=/path/to/socket",
+			"GITALY_TOKEN=secret",
+			"GITALY_REPOSITORY=" + marshalledRepo,
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, HooksPayload{
+			Repo:                repo,
+			BinDir:              "/bin/dir",
+			InternalSocket:      "/path/to/socket",
+			InternalSocketToken: "secret",
+		}, payload)
+	})
+
+	t.Run("fallback without token", func(t *testing.T) {
+		payload, err := HooksPayloadFromEnv([]string{
+			"GITALY_BIN_DIR=/bin/dir",
+			"GITALY_SOCKET=/path/to/socket",
+			"GITALY_REPOSITORY=" + marshalledRepo,
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, HooksPayload{
+			Repo:           repo,
+			BinDir:         "/bin/dir",
+			InternalSocket: "/path/to/socket",
+		}, payload)
+	})
+
+	t.Run("fallback with missing repository", func(t *testing.T) {
+		_, err := HooksPayloadFromEnv([]string{
+			"GITALY_BIN_DIR=/bin/dir",
+			"GITALY_SOCKET=/path/to/socket",
+		})
+		require.Equal(t, ErrPayloadNotFound, err)
+	})
+
+	t.Run("fallback with missing bindir", func(t *testing.T) {
+		_, err := HooksPayloadFromEnv([]string{
+			"GITALY_SOCKET=/path/to/socket",
+			"GITALY_REPOSITORY=" + marshalledRepo,
+		})
+		require.Equal(t, ErrPayloadNotFound, err)
+	})
+
+	t.Run("fallback with missing socket", func(t *testing.T) {
+		_, err := HooksPayloadFromEnv([]string{
+			"GITALY_BIN_DIR=/bin/dir",
+			"GITALY_REPOSITORY=" + marshalledRepo,
+		})
+		require.Equal(t, ErrPayloadNotFound, err)
+	})
 }
