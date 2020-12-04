428c351e1b72b229f8f89b0ab0d573f2207a7bfb
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-12-04 16:19:03 +0100

    hooks: Split up implementation of options and payload
    The hook implementation is getting quite complex, but in fact concerns
    are quite easily separable into command options and the payload. So
    let's split up those two implementations into two separate files to make
    them easier to read.

--- internal/git/hooks_payload.go
+++ internal/git/hooks_payload.go
@@ -1,7 +1,6 @@
 package git
 
 import (
-	"context"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
@@ -9,10 +8,7 @@ import (
 	"strings"
 
 	"github.com/golang/protobuf/jsonpb"
-	"gitlab.com/gitlab-org/gitaly/internal/git/hooks"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
-	"gitlab.com/gitlab-org/gitaly/internal/gitlabshell"
-	"gitlab.com/gitlab-org/gitaly/internal/praefect/metadata"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 )
 
@@ -163,125 +159,4 @@ func (p HooksPayload) Env() (string, error) {
 	encoded := base64.StdEncoding.EncodeToString(marshalled)
 
 	return fmt.Sprintf("%s=%s", ErrHooksPayloadNotFound, encoded), nil
-}
-
-// WithRefTxHook returns an option that populates the safe command with the
-// environment variables necessary to properly execute a reference hook for
-// repository changes that may possibly update references
-func WithRefTxHook(ctx context.Context, repo *gitalypb.Repository, cfg config.Cfg) CmdOpt {
-	return func(cc *cmdCfg) error {
-		if repo == nil {
-			return fmt.Errorf("missing repo: %w", ErrInvalidArg)
-		}
-
-		rfEnvs, err := refHookEnv(ctx, repo, cfg)
-		if err != nil {
-			return fmt.Errorf("ref hook env var: %w", err)
-		}
-		cc.env = append(cc.env, rfEnvs...)
-
-		txEnvs, err := transactionEnv(ctx)
-		if err != nil {
-			return fmt.Errorf("transaction environment: %w", err)
-		}
-		cc.env = append(cc.env, txEnvs...)
-
-		cc.globals = append(cc.globals, ValueFlag{"-c", fmt.Sprintf("core.hooksPath=%s", hooks.Path(cfg))})
-		cc.refHookConfigured = true
-
-		return nil
-	}
-}
-
-// refHookEnv returns all env vars required by the reference transaction hook
-func refHookEnv(ctx context.Context, repo *gitalypb.Repository, cfg config.Cfg) ([]string, error) {
-	payload, err := NewHooksPayload(cfg, repo).Env()
-	if err != nil {
-		return nil, err
-	}
-
-	return []string{
-		payload,
-		"GITALY_BIN_DIR=" + cfg.BinDir,
-	}, nil
-}
-
-// ReceivePackRequest abstracts away the different requests that end up
-// spawning git-receive-pack.
-type ReceivePackRequest interface {
-	GetGlId() string
-	GetGlUsername() string
-	GetGlRepository() string
-	GetRepository() *gitalypb.Repository
-}
-
-// WithReceivePackHooks returns an option that populates the safe command with the environment
-// variables necessary to properly execute the pre-receive, update and post-receive hooks for
-// git-receive-pack(1).
-func WithReceivePackHooks(ctx context.Context, cfg config.Cfg, req ReceivePackRequest, protocol string) CmdOpt {
-	return func(cc *cmdCfg) error {
-		env, err := receivePackHookEnv(ctx, cfg, req, protocol)
-		if err != nil {
-			return fmt.Errorf("receive-pack hook envvars: %w", err)
-		}
-
-		cc.env = append(cc.env, env...)
-		return nil
-	}
-}
-
-func receivePackHookEnv(ctx context.Context, cfg config.Cfg, req ReceivePackRequest, protocol string) ([]string, error) {
-	gitlabshellEnv, err := gitlabshell.EnvFromConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	env, err := refHookEnv(ctx, req.GetRepository(), config.Config)
-	if err != nil {
-		return nil, err
-	}
-
-	env = append(env,
-		fmt.Sprintf("GL_ID=%s", req.GetGlId()),
-		fmt.Sprintf("GL_USERNAME=%s", req.GetGlUsername()),
-		fmt.Sprintf("GL_REPOSITORY=%s", req.GetGlRepository()),
-		fmt.Sprintf("GL_PROJECT_PATH=%s", req.GetRepository().GetGlProjectPath()),
-		fmt.Sprintf("GL_PROTOCOL=%s", protocol),
-	)
-	env = append(env, gitlabshellEnv...)
-
-	transactionEnv, err := transactionEnv(ctx)
-	if err != nil {
-		return nil, err
-	}
-	env = append(env, transactionEnv...)
-
-	return env, nil
-}
-
-func transactionEnv(ctx context.Context) ([]string, error) {
-	transaction, err := metadata.TransactionFromContext(ctx)
-	if err != nil {
-		if errors.Is(err, metadata.ErrTransactionNotFound) {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	praefect, err := metadata.PraefectFromContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	praefectEnv, err := praefect.Env()
-	if err != nil {
-		return nil, err
-	}
-
-	transactionEnv, err := transaction.Env()
-	if err != nil {
-		return nil, err
-	}
-
-	return []string{praefectEnv, transactionEnv}, nil
 }
--- internal/git/hooks_payload_test.go
+++ internal/git/hooks_payload_test.go
@@ -6,74 +6,9 @@ import (
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitaly/internal/command"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 )
-
-func TestWithRefHook(t *testing.T) {
-	testRepo, _, cleanup := testhelper.NewTestRepo(t)
-	defer cleanup()
-
-	ctx, cancel := testhelper.Context()
-	defer cancel()
-
-	const token = "my-super-secure-token"
-	defer func(oldToken string) { config.Config.Auth.Token = oldToken }(config.Config.Auth.Token)
-	config.Config.Auth.Token = token
-
-	opt := WithRefTxHook(ctx, testRepo, config.Config)
-	subCmd := SubCmd{Name: "update-ref", Args: []string{"refs/heads/master", NullSHA}}
-
-	for _, tt := range []struct {
-		name string
-		fn   func() (*command.Command, error)
-	}{
-		{
-			name: "SafeCmd",
-			fn: func() (*command.Command, error) {
-				return SafeCmd(ctx, testRepo, nil, subCmd, opt)
-			},
-		},
-		{
-			name: "SafeCmdWithEnv",
-			fn: func() (*command.Command, error) {
-				return SafeCmdWithEnv(ctx, nil, testRepo, nil, subCmd, opt)
-			},
-		},
-		{
-			name: "SafeStdinCmd",
-			fn: func() (*command.Command, error) {
-				return SafeStdinCmd(ctx, testRepo, nil, subCmd, opt)
-			},
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			cmd, err := tt.fn()
-			require.NoError(t, err)
-			// There is no full setup, so executing the hook will fail.
-			require.Error(t, cmd.Wait())
-
-			var actualEnvVars []string
-			for _, env := range cmd.Env() {
-				kv := strings.SplitN(env, "=", 2)
-				require.Len(t, kv, 2)
-				key, val := kv[0], kv[1]
-
-				if strings.HasPrefix(key, "GL_") || strings.HasPrefix(key, "GITALY_") {
-					require.NotEmptyf(t, strings.TrimSpace(val),
-						"env var %s value should not be empty string", key)
-					actualEnvVars = append(actualEnvVars, key)
-				}
-			}
-
-			require.EqualValues(t, []string{
-				"GITALY_HOOKS_PAYLOAD",
-				"GITALY_BIN_DIR",
-			}, actualEnvVars)
-		})
-	}
-}
 
 func TestHooksPayload(t *testing.T) {
 	repo, _, cleanup := testhelper.NewTestRepo(t)
