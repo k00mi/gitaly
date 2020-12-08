4e0faa566a9b9d789c04c01db4cca2d4a5c524b1
Patrick Steinhardt <psteinhardt@gitlab.com>
2020-12-08 11:06:43 +0100

    git: Unsplit safecmd test package
    The safecmd tests currently use a separate "git_test" package for
    testing, probably because there had been an import cycle in the past.
    There isn't nowadays anymore, so let's merge it into the normal "git"
    package.

--- internal/git/safecmd_test.go
+++ internal/git/safecmd_test.go
@@ -1,4 +1,4 @@
-package git_test
+package git
 
 import (
 	"bytes"
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/git/hooks"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/internal/helper/text"
@@ -17,53 +16,53 @@ import (
 
 func TestFlagValidation(t *testing.T) {
 	for _, tt := range []struct {
-		option git.Option
+		option Option
 		valid  bool
 	}{
 		// valid Flag inputs
-		{option: git.Flag{Name: "-k"}, valid: true},
-		{option: git.Flag{Name: "-K"}, valid: true},
-		{option: git.Flag{Name: "--asdf"}, valid: true},
-		{option: git.Flag{Name: "--asdf-qwer"}, valid: true},
-		{option: git.Flag{Name: "--asdf=qwerty"}, valid: true},
-		{option: git.Flag{Name: "-D=A"}, valid: true},
-		{option: git.Flag{Name: "-D="}, valid: true},
+		{option: Flag{Name: "-k"}, valid: true},
+		{option: Flag{Name: "-K"}, valid: true},
+		{option: Flag{Name: "--asdf"}, valid: true},
+		{option: Flag{Name: "--asdf-qwer"}, valid: true},
+		{option: Flag{Name: "--asdf=qwerty"}, valid: true},
+		{option: Flag{Name: "-D=A"}, valid: true},
+		{option: Flag{Name: "-D="}, valid: true},
 
 		// valid ValueFlag inputs
-		{option: git.ValueFlag{"-k", "adsf"}, valid: true},
-		{option: git.ValueFlag{"-k", "--anything"}, valid: true},
-		{option: git.ValueFlag{"-k", ""}, valid: true},
+		{option: ValueFlag{"-k", "adsf"}, valid: true},
+		{option: ValueFlag{"-k", "--anything"}, valid: true},
+		{option: ValueFlag{"-k", ""}, valid: true},
 
 		// valid SubSubCmd inputs
-		{option: git.SubSubCmd{"meow"}, valid: true},
+		{option: SubSubCmd{"meow"}, valid: true},
 
 		// valid ConfigPair inputs
-		{option: git.ConfigPair{Key: "a.b.c", Value: "d"}, valid: true},
-		{option: git.ConfigPair{Key: "core.sound", Value: "meow"}, valid: true},
-		{option: git.ConfigPair{Key: "asdf-qwer.1234-5678", Value: ""}, valid: true},
-		{option: git.ConfigPair{Key: "http.https://user@example.com/repo.git.user", Value: "kitty"}, valid: true},
+		{option: ConfigPair{Key: "a.b.c", Value: "d"}, valid: true},
+		{option: ConfigPair{Key: "core.sound", Value: "meow"}, valid: true},
+		{option: ConfigPair{Key: "asdf-qwer.1234-5678", Value: ""}, valid: true},
+		{option: ConfigPair{Key: "http.https://user@example.com/repo.git.user", Value: "kitty"}, valid: true},
 
 		// invalid Flag inputs
-		{option: git.Flag{Name: "-*"}},          // invalid character
-		{option: git.Flag{Name: "a"}},           // missing dash
-		{option: git.Flag{Name: "[["}},          // suspicious characters
-		{option: git.Flag{Name: "||"}},          // suspicious characters
-		{option: git.Flag{Name: "asdf=qwerty"}}, // missing dash
+		{option: Flag{Name: "-*"}},          // invalid character
+		{option: Flag{Name: "a"}},           // missing dash
+		{option: Flag{Name: "[["}},          // suspicious characters
+		{option: Flag{Name: "||"}},          // suspicious characters
+		{option: Flag{Name: "asdf=qwerty"}}, // missing dash
 
 		// invalid ValueFlag inputs
-		{option: git.ValueFlag{"k", "asdf"}}, // missing dash
+		{option: ValueFlag{"k", "asdf"}}, // missing dash
 
 		// invalid SubSubCmd inputs
-		{option: git.SubSubCmd{"--meow"}}, // cannot start with dash
+		{option: SubSubCmd{"--meow"}}, // cannot start with dash
 
 		// invalid ConfigPair inputs
-		{option: git.ConfigPair{Key: "", Value: ""}},            // key cannot be empty
-		{option: git.ConfigPair{Key: " ", Value: ""}},           // key cannot be whitespace
-		{option: git.ConfigPair{Key: "asdf", Value: ""}},        // two components required
-		{option: git.ConfigPair{Key: "asdf.", Value: ""}},       // 2nd component must be non-empty
-		{option: git.ConfigPair{Key: "--asdf.asdf", Value: ""}}, // key cannot start with dash
-		{option: git.ConfigPair{Key: "as[[df.asdf", Value: ""}}, // 1st component cannot contain non-alphanumeric
-		{option: git.ConfigPair{Key: "asdf.as]]df", Value: ""}}, // 2nd component cannot contain non-alphanumeric
+		{option: ConfigPair{Key: "", Value: ""}},            // key cannot be empty
+		{option: ConfigPair{Key: " ", Value: ""}},           // key cannot be whitespace
+		{option: ConfigPair{Key: "asdf", Value: ""}},        // two components required
+		{option: ConfigPair{Key: "asdf.", Value: ""}},       // 2nd component must be non-empty
+		{option: ConfigPair{Key: "--asdf.asdf", Value: ""}}, // key cannot start with dash
+		{option: ConfigPair{Key: "as[[df.asdf", Value: ""}}, // 1st component cannot contain non-alphanumeric
+		{option: ConfigPair{Key: "asdf.as]]df", Value: ""}}, // 2nd component cannot contain non-alphanumeric
 	} {
 		args, err := tt.option.ValidateArgs()
 		if tt.valid {
@@ -71,52 +70,52 @@ func TestFlagValidation(t *testing.T) {
 		} else {
 			require.Error(t, err,
 				"expected error, but args %v passed validation", args)
-			require.True(t, git.IsInvalidArgErr(err))
+			require.True(t, IsInvalidArgErr(err))
 		}
 	}
 }
 
 func TestSafeCmdInvalidArg(t *testing.T) {
 	for _, tt := range []struct {
-		globals []git.Option
-		subCmd  git.SubCmd
+		globals []Option
+		subCmd  SubCmd
 		errMsg  string
 	}{
 		{
-			subCmd: git.SubCmd{Name: "--meow"},
+			subCmd: SubCmd{Name: "--meow"},
 			errMsg: `invalid sub command name "--meow": invalid argument`,
 		},
 		{
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name:  "update-ref",
-				Flags: []git.Option{git.Flag{Name: "woof"}},
+				Flags: []Option{Flag{Name: "woof"}},
 			},
 			errMsg: `flag "woof" failed regex validation: invalid argument`,
 		},
 		{
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name: "update-ref",
 				Args: []string{"--tweet"},
 			},
 			errMsg: `positional arg "--tweet" cannot start with dash '-': invalid argument`,
 		},
 		{
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name:  "update-ref",
-				Flags: []git.Option{git.SubSubCmd{"-invalid"}},
+				Flags: []Option{SubSubCmd{"-invalid"}},
 			},
 			errMsg: `invalid sub-sub command name "-invalid": invalid argument`,
 		},
 	} {
-		_, err := git.SafeCmd(
+		_, err := SafeCmd(
 			context.Background(),
 			&gitalypb.Repository{},
 			tt.globals,
 			tt.subCmd,
-			git.WithRefTxHook(context.Background(), &gitalypb.Repository{}, config.Config),
+			WithRefTxHook(context.Background(), &gitalypb.Repository{}, config.Config),
 		)
 		require.EqualError(t, err, tt.errMsg)
-		require.True(t, git.IsInvalidArgErr(err))
+		require.True(t, IsInvalidArgErr(err))
 	}
 }
 
@@ -136,26 +135,26 @@ func TestSafeCmdValid(t *testing.T) {
 
 	for _, tt := range []struct {
 		desc       string
-		globals    []git.Option
-		subCmd     git.SubCmd
+		globals    []Option
+		subCmd     SubCmd
 		expectArgs []string
 	}{
 		{
 			desc:       "no args",
-			subCmd:     git.SubCmd{Name: "update-ref"},
+			subCmd:     SubCmd{Name: "update-ref"},
 			expectArgs: []string{"-c", hooksPath, "update-ref", endOfOptions},
 		},
 		{
 			desc: "single option",
-			globals: []git.Option{
-				git.Flag{Name: "--aaaa-bbbb"},
+			globals: []Option{
+				Flag{Name: "--aaaa-bbbb"},
 			},
-			subCmd:     git.SubCmd{Name: "update-ref"},
+			subCmd:     SubCmd{Name: "update-ref"},
 			expectArgs: []string{"--aaaa-bbbb", "-c", hooksPath, "update-ref", endOfOptions},
 		},
 		{
 			desc: "empty arg and postsep args",
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name:        "update-ref",
 				Args:        []string{""},
 				PostSepArgs: []string{"-woof", ""},
@@ -164,16 +163,16 @@ func TestSafeCmdValid(t *testing.T) {
 		},
 		{
 			desc: "full blown",
-			globals: []git.Option{
-				git.Flag{Name: "-a"},
-				git.ValueFlag{"-b", "c"},
+			globals: []Option{
+				Flag{Name: "-a"},
+				ValueFlag{"-b", "c"},
 			},
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name: "update-ref",
-				Flags: []git.Option{
-					git.Flag{Name: "-e"},
-					git.ValueFlag{"-f", "g"},
-					git.Flag{Name: "-h=i"},
+				Flags: []Option{
+					Flag{Name: "-e"},
+					ValueFlag{"-f", "g"},
+					Flag{Name: "-h=i"},
 				},
 				Args:        []string{"1", "2"},
 				PostSepArgs: []string{"3", "4", "5"},
@@ -182,60 +181,60 @@ func TestSafeCmdValid(t *testing.T) {
 		},
 		{
 			desc: "output to stdout",
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name: "update-ref",
-				Flags: []git.Option{
-					git.SubSubCmd{"verb"},
-					git.OutputToStdout,
-					git.Flag{Name: "--adjective"},
+				Flags: []Option{
+					SubSubCmd{"verb"},
+					OutputToStdout,
+					Flag{Name: "--adjective"},
 				},
 			},
 			expectArgs: []string{"-c", hooksPath, "update-ref", "verb", "-", "--adjective", endOfOptions},
 		},
 		{
 			desc: "multiple value flags",
-			globals: []git.Option{
-				git.Flag{Name: "--contributing"},
-				git.ValueFlag{"--author", "a-gopher"},
+			globals: []Option{
+				Flag{Name: "--contributing"},
+				ValueFlag{"--author", "a-gopher"},
 			},
-			subCmd: git.SubCmd{
+			subCmd: SubCmd{
 				Name: "update-ref",
 				Args: []string{"mr"},
-				Flags: []git.Option{
-					git.Flag{Name: "--is-important"},
-					git.ValueFlag{"--why", "looking-for-first-contribution"},
+				Flags: []Option{
+					Flag{Name: "--is-important"},
+					ValueFlag{"--why", "looking-for-first-contribution"},
 				},
 			},
 			expectArgs: []string{"--contributing", "--author", "a-gopher", "-c", hooksPath, "update-ref", "--is-important", "--why", "looking-for-first-contribution", "mr", endOfOptions},
 		},
 	} {
 		t.Run(tt.desc, func(t *testing.T) {
-			opts := []git.CmdOpt{git.WithRefTxHook(ctx, &gitalypb.Repository{}, config.Config)}
+			opts := []CmdOpt{WithRefTxHook(ctx, &gitalypb.Repository{}, config.Config)}
 
-			cmd, err := git.SafeCmd(ctx, testRepo, tt.globals, tt.subCmd, opts...)
+			cmd, err := SafeCmd(ctx, testRepo, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			// ignore first 3 indeterministic args (executable path and repo args)
 			require.Equal(t, tt.expectArgs, cmd.Args()[3:])
 
-			cmd, err = git.SafeCmdWithEnv(ctx, nil, testRepo, tt.globals, tt.subCmd, opts...)
+			cmd, err = SafeCmdWithEnv(ctx, nil, testRepo, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			// ignore first 3 indeterministic args (executable path and repo args)
 			require.Equal(t, tt.expectArgs, cmd.Args()[3:])
 
-			cmd, err = git.SafeStdinCmd(ctx, testRepo, tt.globals, tt.subCmd, opts...)
+			cmd, err = SafeStdinCmd(ctx, testRepo, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			require.Equal(t, tt.expectArgs, cmd.Args()[3:])
 
-			cmd, err = git.SafeBareCmd(ctx, git.CmdStream{}, nil, tt.globals, tt.subCmd, opts...)
+			cmd, err = SafeBareCmd(ctx, CmdStream{}, nil, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			// ignore first indeterministic arg (executable path)
 			require.Equal(t, tt.expectArgs, cmd.Args()[1:])
 
-			cmd, err = git.SafeCmdWithoutRepo(ctx, git.CmdStream{}, tt.globals, tt.subCmd, opts...)
+			cmd, err = SafeCmdWithoutRepo(ctx, CmdStream{}, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			require.Equal(t, tt.expectArgs, cmd.Args()[1:])
 
-			cmd, err = git.SafeBareCmdInDir(ctx, testRepoPath, git.CmdStream{}, nil, tt.globals, tt.subCmd, opts...)
+			cmd, err = SafeBareCmdInDir(ctx, testRepoPath, CmdStream{}, nil, tt.globals, tt.subCmd, opts...)
 			require.NoError(t, err)
 			require.Equal(t, tt.expectArgs, cmd.Args()[1:])
 		})
@@ -252,18 +251,18 @@ func TestSafeCmdWithEnv(t *testing.T) {
 	reenableGitCmd := disableGitCmd()
 	defer reenableGitCmd()
 
-	globals := []git.Option{
-		git.Flag{Name: "--aaaa-bbbb"},
+	globals := []Option{
+		Flag{Name: "--aaaa-bbbb"},
 	}
 
-	subCmd := git.SubCmd{Name: "update-ref"}
+	subCmd := SubCmd{Name: "update-ref"}
 	endOfOptions := "--end-of-options"
 	expectArgs := []string{"--aaaa-bbbb", "-c", "core.hooksPath=" + hooks.Path(config.Config), "update-ref", endOfOptions}
 
 	env := []string{"TEST_VAR1=1", "TEST_VAR2=2"}
 
-	opts := []git.CmdOpt{git.WithRefTxHook(ctx, &gitalypb.Repository{}, config.Config)}
-	cmd, err := git.SafeCmdWithEnv(ctx, env, testRepo, globals, subCmd, opts...)
+	opts := []CmdOpt{WithRefTxHook(ctx, &gitalypb.Repository{}, config.Config)}
+	cmd, err := SafeCmdWithEnv(ctx, env, testRepo, globals, subCmd, opts...)
 	require.NoError(t, err)
 	// ignore first 3 indeterministic args (executable path and repo args)
 	require.Equal(t, expectArgs, cmd.Args()[3:])
@@ -281,7 +280,7 @@ func TestSafeBareCmdInDir(t *testing.T) {
 		ctx, cancel := testhelper.Context()
 		defer cancel()
 
-		_, err := git.SafeBareCmdInDir(ctx, "", git.CmdStream{}, nil, nil, nil)
+		_, err := SafeBareCmdInDir(ctx, "", CmdStream{}, nil, nil, nil)
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "no 'dir' provided")
 	})
@@ -294,7 +293,7 @@ func TestSafeBareCmdInDir(t *testing.T) {
 		defer cancel()
 
 		var stderr bytes.Buffer
-		cmd, err := git.SafeBareCmdInDir(ctx, repoPath, git.CmdStream{Err: &stderr}, nil, nil, git.SubCmd{
+		cmd, err := SafeBareCmdInDir(ctx, repoPath, CmdStream{Err: &stderr}, nil, nil, SubCmd{
 			Name: "rev-parse",
 			Args: []string{"master"},
 		})
@@ -313,7 +312,7 @@ func TestSafeBareCmdInDir(t *testing.T) {
 		defer cancel()
 
 		var stderr bytes.Buffer
-		_, err := git.SafeBareCmdInDir(ctx, "non-existing-dir", git.CmdStream{Err: &stderr}, nil, nil, git.SubCmd{
+		_, err := SafeBareCmdInDir(ctx, "non-existing-dir", CmdStream{Err: &stderr}, nil, nil, SubCmd{
 			Name: "rev-parse",
 			Args: []string{"master"},
 		})
@@ -331,12 +330,12 @@ func TestSafeCmd(t *testing.T) {
 		defer cancel()
 
 		var stderr bytes.Buffer
-		cmd, err := git.SafeCmd(ctx, repo, nil,
-			git.SubCmd{
+		cmd, err := SafeCmd(ctx, repo, nil,
+			SubCmd{
 				Name: "rev-parse",
 				Args: []string{"master"},
 			},
-			git.WithStderr(&stderr),
+			WithStderr(&stderr),
 		)
 		require.NoError(t, err)
 
@@ -357,11 +356,11 @@ func TestSafeCmd(t *testing.T) {
 		defer cancel()
 
 		var stderr bytes.Buffer
-		cmd, err := git.SafeCmd(ctx, repo, nil, git.SubCmd{
+		cmd, err := SafeCmd(ctx, repo, nil, SubCmd{
 			Name: "rev-parse",
 			Args: []string{"invalid-ref"},
 		},
-			git.WithStderr(&stderr),
+			WithStderr(&stderr),
 		)
 		require.NoError(t, err)
 
